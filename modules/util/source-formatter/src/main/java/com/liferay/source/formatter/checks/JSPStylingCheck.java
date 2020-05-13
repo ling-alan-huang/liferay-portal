e4eacba1d9b289c7276a6fbac8c34e82548e86a6
Alan Huang <alan.huang@liferay.com>
2020-05-13 13:53:25 +0800

    LPS-84119 Use the implicit var liferayPortletResponse

--- modules/util/source-formatter/src/main/java/com/liferay/source/formatter/checks/JSPStylingCheck.java
+++ modules/util/source-formatter/src/main/java/com/liferay/source/formatter/checks/JSPStylingCheck.java
@@ -37,6 +37,8 @@ public class JSPStylingCheck extends BaseStylingCheck {
 
 		content = _fixIncorrectClosingTag(content);
 
+		content = _fixIncorrectMethodCall(fileName, content);
+
 		content = _fixIncorrectSingleLineJavaSource(content);
 
 		content = StringUtil.replace(
@@ -83,6 +85,20 @@ public class JSPStylingCheck extends BaseStylingCheck {
 			return StringUtil.replaceFirst(
 				content, " />\n", "\n" + matcher.group(1) + "/>\n",
 				matcher.end(1));
+		}
+
+		return content;
+	}
+
+	private String _fixIncorrectMethodCall(String fileName, String content) {
+		Matcher matcher = _incorrectMethodCallPattern.matcher(content);
+
+		while (matcher.find()) {
+			addMessage(
+				fileName,
+				"Use 'liferayPortletResponse.getNamespace()' instead of " +
+					"'*Response.getNamespace()'",
+				getLineNumber(content, matcher.start()));
 		}
 
 		return content;
@@ -153,6 +169,8 @@ public class JSPStylingCheck extends BaseStylingCheck {
 		"=(\n\\s*).*;\n");
 	private static final Pattern _incorrectLineBreakPattern3 = Pattern.compile(
 		"(\n(\t*)<(\\w+)>)(<\\w+>.*)(</\\3>\n)");
+	private static final Pattern _incorrectMethodCallPattern = Pattern.compile(
+		"\\b(?!liferayPortlet)\\w+Response\\.getNamespace\\(\\)");
 	private static final Pattern _incorrectSingleLineJavaSourcePattern =
 		Pattern.compile("(\t*)(<% (.*) %>)\n");
 
