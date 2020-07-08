faee14b3cd3bbb01854d1a75817c53c0bf8d3e29
Alan Huang <alan.huang@liferay.com>
2020-07-08 14:02:33 +0800

    LPS-105380 Fix violations

--- modules/apps/account/account-admin-web/src/main/java/com/liferay/account/admin/web/internal/frontend/taglib/servlet/taglib/ui/AccountEntryOrganizationsScreenNavigationCategory.java
+++ modules/apps/account/account-admin-web/src/main/java/com/liferay/account/admin/web/internal/frontend/taglib/servlet/taglib/ui/AccountEntryOrganizationsScreenNavigationCategory.java
@@ -12,14 +12,20 @@
  * details.
  */
 
-package com.liferay.account.admin.web.internal.frontend.taglib.servlet.taglib;
+package com.liferay.account.admin.web.internal.frontend.taglib.servlet.taglib.ui;
 
 import com.liferay.account.admin.web.internal.constants.AccountScreenNavigationEntryConstants;
 import com.liferay.account.admin.web.internal.display.AccountEntryDisplay;
+import com.liferay.account.admin.web.internal.security.permission.resource.AccountEntryPermission;
+import com.liferay.account.constants.AccountActionKeys;
 import com.liferay.frontend.taglib.servlet.taglib.ScreenNavigationCategory;
 import com.liferay.frontend.taglib.servlet.taglib.ScreenNavigationEntry;
+import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.language.LanguageUtil;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.model.User;
+import com.liferay.portal.kernel.security.permission.PermissionCheckerFactoryUtil;
 
 import java.util.Locale;
 
@@ -30,44 +36,63 @@ import org.osgi.service.component.annotations.Component;
  */
 @Component(
 	property = {
-		"screen.navigation.category.order:Integer=60",
+		"screen.navigation.category.order:Integer=30",
 		"screen.navigation.entry.order:Integer=10"
 	},
 	service = {ScreenNavigationCategory.class, ScreenNavigationEntry.class}
 )
-public class AccountEntryRolesScreenNavigationCategory
+public class AccountEntryOrganizationsScreenNavigationCategory
 	extends BaseAccountEntryScreenNavigationEntry
 	implements ScreenNavigationCategory {
 
 	@Override
 	public String getCategoryKey() {
-		return AccountScreenNavigationEntryConstants.CATEGORY_KEY_ROLES;
+		return AccountScreenNavigationEntryConstants.CATEGORY_KEY_ORGANIZATIONS;
 	}
 
 	@Override
 	public String getEntryKey() {
-		return AccountScreenNavigationEntryConstants.ENTRY_KEY_ROLES;
+		return AccountScreenNavigationEntryConstants.ENTRY_KEY_ORGANIZATIONS;
 	}
 
 	@Override
 	public String getJspPath() {
-		return "/account_entries_admin/account_entry/view_account_roles.jsp";
+		return "/account_entries_admin/account_entry" +
+			"/view_account_organizations.jsp";
 	}
 
 	@Override
 	public String getLabel(Locale locale) {
-		return LanguageUtil.get(locale, "roles");
+		return LanguageUtil.get(locale, "organizations");
 	}
 
 	@Override
 	public boolean isVisible(
 		User user, AccountEntryDisplay accountEntryDisplay) {
 
-		if (accountEntryDisplay.getAccountEntryId() > 0) {
-			return true;
+		if (accountEntryDisplay.getAccountEntryId() == 0) {
+			return false;
+		}
+
+		try {
+			if (AccountEntryPermission.contains(
+					PermissionCheckerFactoryUtil.create(user),
+					accountEntryDisplay.getAccountEntryId(),
+					AccountActionKeys.VIEW_ORGANIZATIONS)) {
+
+				return true;
+			}
+		}
+		catch (PortalException portalException) {
+			if (_log.isDebugEnabled()) {
+				_log.debug(portalException, portalException);
+			}
 		}
 
 		return false;
 	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		AccountEntryOrganizationsScreenNavigationCategory.class);
 
 }
