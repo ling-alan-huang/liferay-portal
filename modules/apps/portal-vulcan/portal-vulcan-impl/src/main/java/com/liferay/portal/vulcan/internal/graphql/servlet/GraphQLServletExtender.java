cf53d9bd0706f53cd1514624d2cee12b6c5cdcf4
Alan Huang <alan.huang@liferay.com>
2020-08-18 18:54:16 +0800

    LPS-108380 SF, use String.valueOf

--- modules/apps/portal-vulcan/portal-vulcan-impl/src/main/java/com/liferay/portal/vulcan/internal/graphql/servlet/GraphQLServletExtender.java
+++ modules/apps/portal-vulcan/portal-vulcan-impl/src/main/java/com/liferay/portal/vulcan/internal/graphql/servlet/GraphQLServletExtender.java
@@ -1123,9 +1123,7 @@ public class GraphQLServletExtender {
 	private Integer _getVersion(Method method) {
 		Class<?> clazz = method.getDeclaringClass();
 
-		Package pkg = clazz.getPackage();
-
-		String packageString = pkg.toString();
+		String packageString = String.valueOf(clazz.getPackage());
 
 		String[] packageNames = packageString.split("\\.");
 
