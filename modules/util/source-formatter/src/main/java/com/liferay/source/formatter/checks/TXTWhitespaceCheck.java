312ca2daa2878ea856e2fccc5af5faff42a3ab47
Alan Huang <alan.huang@liferay.com>
2020-10-29 11:39:51 +0800

    LPS-105380 Add TXTWhitespaceCheck

---
+++ modules/util/source-formatter/src/main/java/com/liferay/source/formatter/checks/TXTWhitespaceCheck.java
@@ -0,0 +1,40 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.source.formatter.checks;
+
+import java.io.IOException;
+
+/**
+ * @author Alan Huang
+ */
+public class TXTWhitespaceCheck extends WhitespaceCheck {
+
+	@Override
+	protected String doProcess(
+			String fileName, String absolutePath, String content)
+		throws IOException {
+
+		if (!absolutePath.endsWith("find-security-bugs-false-positives.txt") &&
+			!absolutePath.matches(
+				".*\\/modules\\/third-party\\/com-h3xstream-findsecbugs\\/" +
+					"src\\/main\\/resources\\/liferay-config\\/\\.txt")) {
+
+			return content;
+		}
+
+		return super.doProcess(fileName, absolutePath, content);
+	}
+
+}
