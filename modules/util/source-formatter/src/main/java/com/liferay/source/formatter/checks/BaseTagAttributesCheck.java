acf976cbf326bc253ce43dbca80c6d72bc9bb863
Alan Huang <alan.huang@liferay.com>
2020-12-24 13:58:58 +0800

    LPS-105380 Fix regex

--- modules/util/source-formatter/src/main/java/com/liferay/source/formatter/checks/BaseTagAttributesCheck.java
+++ modules/util/source-formatter/src/main/java/com/liferay/source/formatter/checks/BaseTagAttributesCheck.java
@@ -489,7 +489,7 @@ public abstract class BaseTagAttributesCheck extends BaseFileCheck {
 	}
 
 	private static final Pattern _attributeNamePattern = Pattern.compile(
-		"[a-z]+[-_:a-zA-Z0-9]*");
+		"[\\w]+[-_:a-zA-Z0-9]*");
 	private static final Pattern _incorrectLineBreakPattern = Pattern.compile(
 		"\n(\t*)(<\\w[-_:\\w]*) (.*)([\"']|%=)\n[\\s\\S]*?>\n");
 	private static final Pattern _multilineTagPattern = Pattern.compile(
