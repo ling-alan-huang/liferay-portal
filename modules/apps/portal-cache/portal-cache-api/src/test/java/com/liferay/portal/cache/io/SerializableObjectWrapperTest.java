66657fb8206e1e992112379e3ab4d4941fc4ff80
Matthew Tambara <matthew.tambara@liferay.com>
2021-03-05 16:28:33 -0800

    LPS-105380 Modules: Manually add where CodeCoverageAssertor is only other test rule

--- modules/apps/portal-cache/portal-cache-api/src/test/java/com/liferay/portal/cache/io/SerializableObjectWrapperTest.java
+++ modules/apps/portal-cache/portal-cache-api/src/test/java/com/liferay/portal/cache/io/SerializableObjectWrapperTest.java
@@ -19,7 +19,9 @@ import com.liferay.petra.io.unsync.UnsyncByteArrayOutputStream;
 import com.liferay.petra.lang.ClassLoaderPool;
 import com.liferay.portal.kernel.test.CaptureHandler;
 import com.liferay.portal.kernel.test.JDKLoggerTestUtil;
+import com.liferay.portal.kernel.test.rule.AggregateTestRule;
 import com.liferay.portal.kernel.test.rule.CodeCoverageAssertor;
+import com.liferay.portal.test.rule.LiferayUnitTestRule;
 
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
@@ -32,6 +34,7 @@ import java.util.logging.LogRecord;
 
 import org.junit.Assert;
 import org.junit.ClassRule;
+import org.junit.Rule;
 import org.junit.Test;
 
 /**
@@ -40,8 +43,10 @@ import org.junit.Test;
 public class SerializableObjectWrapperTest {
 
 	@ClassRule
-	public static final CodeCoverageAssertor codeCoverageAssertor =
-		CodeCoverageAssertor.INSTANCE;
+	@Rule
+	public static final AggregateTestRule aggregateTestRule =
+		new AggregateTestRule(
+			CodeCoverageAssertor.INSTANCE, LiferayUnitTestRule.INSTANCE);
 
 	@Test
 	public void testEquals() throws Exception {
