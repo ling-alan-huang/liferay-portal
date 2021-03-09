8769ae627a52fc8f8f021086ed3af9f70234da3d
Matthew Tambara <matthew.tambara@liferay.com>
2021-03-08 17:05:37 -0800

    LPS-127030 Apply usages

--- portal-impl/test/unit/com/liferay/portal/nio/intraband/proxy/IntrabandProxyUtilTest.java
+++ portal-impl/test/unit/com/liferay/portal/nio/intraband/proxy/IntrabandProxyUtilTest.java
@@ -53,7 +53,7 @@ import com.liferay.portal.kernel.util.SystemProperties;
 import com.liferay.portal.kernel.util.TextFormatter;
 import com.liferay.portal.test.aspects.ReflectionUtilAdvice;
 import com.liferay.portal.test.rule.AdviseWith;
-import com.liferay.portal.test.rule.AspectJNewEnvTestRule;
+import com.liferay.portal.test.rule.LiferayUnitTestRule;
 import com.liferay.portal.util.FileImpl;
 import com.liferay.portal.util.PropsValues;
 
@@ -127,7 +127,7 @@ public class IntrabandProxyUtilTest {
 	@Rule
 	public static final AggregateTestRule aggregateTestRule =
 		new AggregateTestRule(
-			AspectJNewEnvTestRule.INSTANCE, CodeCoverageAssertor.INSTANCE);
+			CodeCoverageAssertor.INSTANCE, LiferayUnitTestRule.INSTANCE);
 
 	@Before
 	public void setUp() {
