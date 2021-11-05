f69e2b268e8b1aee8b96e4d16ec4fdfa76275402
Alan Huang <alan.huang@liferay.com>
2021-11-05 17:06:27 +0800

    Revert "compare"
    This reverts commit 1772e0684b72b914afafac1fc526a0e680b10b0c.

--- modules/util/source-formatter/src/main/java/com/liferay/source/formatter/PoshiSourceProcessor.java
+++ modules/util/source-formatter/src/main/java/com/liferay/source/formatter/PoshiSourceProcessor.java
@@ -24,14 +24,13 @@ import com.liferay.source.formatter.util.DebugUtil;
 
 import java.io.File;
 import java.io.IOException;
+
 import java.net.URL;
-import java.nio.file.Files;
-import java.nio.file.Paths;
+
 import java.util.List;
 import java.util.Set;
 
 import org.dom4j.Document;
-import org.dom4j.DocumentException;
 import org.dom4j.Element;
 import org.dom4j.util.NodeComparator;
 
@@ -68,23 +67,19 @@ public class PoshiSourceProcessor extends BaseSourceProcessor {
 			Set<String> modifiedMessages)
 		throws Exception {
 
-		if (fileName.endsWith("JSONWebcontentUtil.macro") || fileName.endsWith("Kaleodesigner.testcase")) {
-			return content;
-		}
 		String orignalPoshiSyntax = _generatePoshiXMLFile(fileName);
-		
+
 		if (orignalPoshiSyntax.length() == 0) {
 			return content;
 		}
-		
-		
+
 		Element expectedElement = _getDom4JElement(orignalPoshiSyntax);
-		
+
 		PoshiElement actualElement = _getPoshiElement(fileName);
 
-			_assertEqualElements(
-					fileName, actualElement, expectedElement,
-					"Poshi script syntax does not translate to Poshi XML");
+		_assertEqualElements(
+			actualElement, expectedElement,
+			"Poshi script syntax does not translate to Poshi XML");
 
 		String newContent = actualElement.toPoshiScript();
 
@@ -101,9 +96,9 @@ public class PoshiSourceProcessor extends BaseSourceProcessor {
 
 		return newContent;
 	}
-	
+
 	private void _assertEqualElements(
-			String fileName, Element actualElement, Element expectedElement, String errorMessage)
+			Element actualElement, Element expectedElement, String errorMessage)
 		throws Exception {
 
 		NodeComparator nodeComparator = new NodeComparator();
@@ -114,12 +109,30 @@ public class PoshiSourceProcessor extends BaseSourceProcessor {
 			String actual = Dom4JUtil.format(actualElement);
 			String expected = Dom4JUtil.format(expectedElement);
 
-//			throw new Exception(errorMessage);
-			System.out.println("====" + fileName);
+			throw new Exception(errorMessage);
 		}
 	}
 
-	private Element _getDom4JElement(String orignalPoshiSyntax) throws Exception {
+	private String _generatePoshiXMLFile(String filePath)
+		throws PoshiScriptParserException {
+
+		try {
+			URL url = FileUtil.getURL(new File(filePath));
+
+			PoshiElement poshiElement =
+				(PoshiElement)PoshiNodeFactory.newPoshiNodeFromFile(url);
+
+			return Dom4JUtil.format(poshiElement);
+		}
+		catch (IOException ioException) {
+			ioException.printStackTrace();
+		}
+
+		return "";
+	}
+
+	private Element _getDom4JElement(String orignalPoshiSyntax)
+		throws Exception {
 
 		Document document = Dom4JUtil.parse(orignalPoshiSyntax);
 
@@ -129,26 +142,6 @@ public class PoshiSourceProcessor extends BaseSourceProcessor {
 
 		return rootElement;
 	}
-
-	private String _generatePoshiXMLFile(String filePath)
-			throws PoshiScriptParserException {
-
-			try {
-				URL url = FileUtil.getURL(new File(filePath));
-
-				PoshiElement poshiElement =
-					(PoshiElement)PoshiNodeFactory.newPoshiNodeFromFile(url);
-
-				
-					return Dom4JUtil.format(
-						poshiElement
-					);
-			}
-			catch (IOException ioException) {
-				ioException.printStackTrace();
-			}
-			return "";
-		}
 
 	private File _getFile(String absolutePath) {
 		return new File(absolutePath);
