47953f09746b4a6af2292197d51993c3270b83d9
Hugo Huijser <hugo.huijser@liferay.com>
2021-11-11 10:03:19 -0700

    LPS-105380 Simplify

--- modules/util/source-formatter/src/main/java/com/liferay/source/formatter/checks/util/YMLSourceUtil.java
+++ modules/util/source-formatter/src/main/java/com/liferay/source/formatter/checks/util/YMLSourceUtil.java
@@ -17,6 +17,7 @@ package com.liferay.source.formatter.checks.util;
 import com.liferay.petra.string.StringBundler;
 import com.liferay.petra.string.StringPool;
 import com.liferay.portal.kernel.util.StringUtil;
+import com.liferay.portal.kernel.util.Validator;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -66,15 +67,7 @@ public class YMLSourceUtil {
 		StringBundler sb = new StringBundler();
 
 		for (String line : lines) {
-			if (line.length() == 0) {
-				sb.append("\n");
-
-				continue;
-			}
-
-			if (line.matches(" +")) {
-				sb.append(line);
-
+			if (Validator.isNull(line)) {
 				sb.append("\n");
 
 				continue;
