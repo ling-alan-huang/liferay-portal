d6a3cef4e277a7fa4a9bb0e18149948c23918ce1
Alan Huang <alan.huang@liferay.com>
2021-12-24 11:39:10 +0800

    LPS-144124 Auto SF

--- modules/util/source-formatter/src/main/java/com/liferay/source/formatter/BaseSourceProcessor.java
+++ modules/util/source-formatter/src/main/java/com/liferay/source/formatter/BaseSourceProcessor.java
@@ -53,7 +53,6 @@ import java.nio.charset.CodingErrorAction;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
@@ -807,7 +806,7 @@ public abstract class BaseSourceProcessor implements SourceProcessor {
 	private SourceFormatterArgs _sourceFormatterArgs;
 	private SourceFormatterConfiguration _sourceFormatterConfiguration;
 	private SourceFormatterExcludes _sourceFormatterExcludes;
-	private Map<String, Set<SourceFormatterMessage>>
+	private final Map<String, Set<SourceFormatterMessage>>
 		_sourceFormatterMessagesMap = new ConcurrentHashMap<>();
 	private SourceFormatterSuppressions _sourceFormatterSuppressions;
 	private final List<SourceMismatchException> _sourceMismatchExceptions =
