ec8767ed401931e48a94feed3a7e9baf8fdb897e
Alan Huang <alan.huang@liferay.com>
2022-02-21 08:23:49 +0800

    LPS-147608 SF, remove unused variables

--- modules/apps/commerce/commerce-product-test/src/testIntegration/java/com/liferay/commerce/product/search/test/CPInstanceIndexerTest.java
+++ modules/apps/commerce/commerce-product-test/src/testIntegration/java/com/liferay/commerce/product/search/test/CPInstanceIndexerTest.java
@@ -22,7 +22,6 @@ import com.liferay.commerce.product.service.CPInstanceLocalService;
 import com.liferay.commerce.product.service.CommerceCatalogLocalService;
 import com.liferay.commerce.product.test.util.CPTestUtil;
 import com.liferay.portal.kernel.model.Group;
-import com.liferay.portal.kernel.model.User;
 import com.liferay.portal.kernel.search.Document;
 import com.liferay.portal.kernel.search.Field;
 import com.liferay.portal.kernel.search.Hits;
@@ -36,7 +35,6 @@ import com.liferay.portal.kernel.test.rule.DeleteAfterTestRun;
 import com.liferay.portal.kernel.test.util.GroupTestUtil;
 import com.liferay.portal.kernel.test.util.RandomTestUtil;
 import com.liferay.portal.kernel.test.util.ServiceContextTestUtil;
-import com.liferay.portal.kernel.test.util.UserTestUtil;
 import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.LocaleUtil;
 import com.liferay.portal.test.rule.Inject;
@@ -70,8 +68,6 @@ public class CPInstanceIndexerTest {
 	@Before
 	public void setUp() throws Exception {
 		_group = GroupTestUtil.addGroup();
-
-		_user = UserTestUtil.addUser();
 
 		_indexer = _indexerRegistry.getIndexer(CPInstance.class);
 	}
@@ -183,8 +179,6 @@ public class CPInstanceIndexerTest {
 
 	@Inject
 	private static IndexerRegistry _indexerRegistry;
-
-	private static User _user;
 
 	@Inject
 	private CommerceCatalogLocalService _commerceCatalogLocalService;
