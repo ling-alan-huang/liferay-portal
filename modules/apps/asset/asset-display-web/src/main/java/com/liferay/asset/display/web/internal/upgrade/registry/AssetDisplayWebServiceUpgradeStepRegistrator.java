bdfa02db5f590476374833db6dd0793866dd6f38
Alan Huang <alan.huang@liferay.com>
2022-06-21 20:22:54 +0800

    LPS-156395 Move packages and rename classes

--- modules/apps/asset/asset-display-web/src/main/java/com/liferay/asset/display/web/internal/upgrade/registry/AssetDisplayWebServiceUpgradeStepRegistrator.java
+++ modules/apps/asset/asset-display-web/src/main/java/com/liferay/asset/display/web/internal/upgrade/registry/AssetDisplayWebServiceUpgradeStepRegistrator.java
@@ -12,7 +12,7 @@
  * details.
  */
 
-package com.liferay.asset.display.web.internal.upgrade;
+package com.liferay.asset.display.web.internal.upgrade.registry;
 
 import com.liferay.portal.kernel.upgrade.DummyUpgradeStep;
 import com.liferay.portal.upgrade.registry.UpgradeStepRegistrator;
@@ -23,7 +23,8 @@ import org.osgi.service.component.annotations.Component;
  * @author Eudaldo Alonso
  */
 @Component(immediate = true, service = UpgradeStepRegistrator.class)
-public class AssetDisplayWebUpgrade implements UpgradeStepRegistrator {
+public class AssetDisplayWebServiceUpgradeStepRegistrator
+	implements UpgradeStepRegistrator {
 
 	@Override
 	public void register(Registry registry) {
