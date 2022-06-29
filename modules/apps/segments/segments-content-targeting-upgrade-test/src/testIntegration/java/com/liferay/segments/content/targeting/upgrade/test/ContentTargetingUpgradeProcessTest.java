1f3d514a6d7c2d7e50f4921c1487bbb7ef68984b
Alan Huang <alan.huang@liferay.com>
2022-06-29 16:18:51 +0800

    LPS-156395 Move packages and rename class names

--- modules/apps/segments/segments-content-targeting-upgrade-test/src/testIntegration/java/com/liferay/segments/content/targeting/upgrade/test/ContentTargetingUpgradeProcessTest.java
+++ modules/apps/segments/segments-content-targeting-upgrade-test/src/testIntegration/java/com/liferay/segments/content/targeting/upgrade/test/ContentTargetingUpgradeProcessTest.java
@@ -818,7 +818,7 @@ public class ContentTargetingUpgradeProcessTest {
 	private SegmentsEntryLocalService _segmentsEntryLocalService;
 
 	@Inject(
-		filter = "component.name=com.liferay.segments.content.targeting.upgrade.internal.SegmentsContentTargetingUpgrade"
+		filter = "component.name=com.liferay.segments.content.targeting.internal.upgrade.registry.SegmentsContentTargetingUpgradeStepRegistrator"
 	)
 	private UpgradeStepRegistrator _upgradeStepRegistror;
 
