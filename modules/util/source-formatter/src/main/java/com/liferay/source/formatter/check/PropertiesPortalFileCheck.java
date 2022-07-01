f1be349c251b75a05d1d77767893ba3fa2a758f6
Alan Huang <alan.huang@liferay.com>
2022-07-01 21:37:38 +0800

    LPS-157417 Remove duplicates

--- modules/util/source-formatter/src/main/java/com/liferay/source/formatter/check/PropertiesPortalFileCheck.java
+++ modules/util/source-formatter/src/main/java/com/liferay/source/formatter/check/PropertiesPortalFileCheck.java
@@ -19,18 +19,33 @@ import com.liferay.petra.string.StringBundler;
 import com.liferay.petra.string.StringPool;
 import com.liferay.portal.kernel.io.unsync.UnsyncBufferedReader;
 import com.liferay.portal.kernel.io.unsync.UnsyncStringReader;
+import com.liferay.portal.kernel.util.ArrayUtil;
 import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portal.kernel.util.Validator;
 import com.liferay.source.formatter.processor.PropertiesSourceProcessor;
+import com.liferay.source.formatter.util.FileUtil;
 
+import java.io.File;
 import java.io.IOException;
 
 import java.net.URL;
 
+import java.nio.file.FileVisitOption;
+import java.nio.file.FileVisitResult;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
+
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
+import java.util.EnumSet;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.apache.commons.io.IOUtils;
 
@@ -48,6 +63,8 @@ public class PropertiesPortalFileCheck extends BaseFileCheck {
 			 fileName.matches(".*/portal(-[^-/]+)*\\.properties")) ||
 			(!isPortalSource() && !isSubrepository() &&
 			 fileName.endsWith("portal.properties"))) {
+
+			content = _generateFeatureFlags(absolutePath, content);
 
 			content = _sortPortalProperties(absolutePath, content);
 
@@ -94,6 +111,70 @@ public class PropertiesPortalFileCheck extends BaseFileCheck {
 		if (content.endsWith("\n")) {
 			content = content.substring(0, content.length() - 1);
 		}
+
+		return content;
+	}
+
+	private String _generateFeatureFlags(String absolutePath, String content)
+		throws IOException {
+
+		File portalDir = getPortalDir();
+
+		final List<File> files = new ArrayList<>();
+
+		List<String> featureFlags = new ArrayList<>();
+
+		Files.walkFileTree(
+			portalDir.toPath(), EnumSet.noneOf(FileVisitOption.class), 25,
+			new SimpleFileVisitor<Path>() {
+
+				@Override
+				public FileVisitResult preVisitDirectory(
+						Path dirPath, BasicFileAttributes basicFileAttributes)
+					throws IOException {
+
+					if (ArrayUtil.contains(
+							_SKIP_DIR_NAMES,
+							String.valueOf(dirPath.getFileName()))) {
+
+						return FileVisitResult.SKIP_SUBTREE;
+					}
+
+					return FileVisitResult.CONTINUE;
+				}
+
+				@Override
+				public FileVisitResult visitFile(
+					Path filePath, BasicFileAttributes basicFileAttributes) {
+
+					files.add(filePath.toFile());
+
+					return FileVisitResult.CONTINUE;
+				}
+
+			});
+
+		long start = System.currentTimeMillis();
+
+		for (File file : files) {
+			String javaContent = FileUtil.read(file);
+
+			if (!javaContent.contains("\"feature.flag.")) {
+				continue;
+			}
+
+			Matcher matcher = _featureFlagPattern.matcher(javaContent);
+
+			while (matcher.find()) {
+				featureFlags.add(matcher.group(1));
+			}
+		}
+
+		Collections.sort(featureFlags);
+
+		long end = System.currentTimeMillis();
+
+		System.out.println("##### " + (end - start));
 
 		return content;
 	}
@@ -315,6 +396,18 @@ public class PropertiesPortalFileCheck extends BaseFileCheck {
 
 	private static final String _ALLOWED_SINGLE_LINE_PROPERTY_KEYS =
 		"allowedSingleLinePropertyKeys";
+
+	private static final String[] _SKIP_DIR_NAMES = {
+		".git", ".gradle", ".idea", ".m2", ".releng", ".settings", "bin",
+		"build", "classes", "node_modules", "node_modules_cache", "sdk",
+		"poshi", "sql", "source-formatter", "test", "test-classes",
+		"test-coverage", "test-results", "tmp"
+	};
+
+	private static final Pattern _featureFlagPattern = Pattern.compile(
+		"\"(feature\\.flag\\..+?)\"");
+	private static final Pattern _featureFlagsPattern = Pattern.compile(
+		"\n##\n## Feature Flag\n##\n\n[\\s\\S]*");
 
 	private String _portalPortalPropertiesContent;
 
