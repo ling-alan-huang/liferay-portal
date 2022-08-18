58260e4bde49617066690f1f2f42c4a4d66e4d6c
Seiphon Wang <seiphon.wang@liferay.com>
2022-08-18 16:58:06 +0800

    LPS-148907 Add more kind of gradle dependencies

---
+++ modules/util/source-formatter/src/main/java/com/liferay/source/formatter/gradle/MethodGradleDependency.java
@@ -0,0 +1,47 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+
+package com.liferay.source.formatter.gradle;
+
+import com.liferay.source.formatter.upgrade.GradleDependency;
+
+/**
+ * @author Seiphon Wang
+ */
+public class MethodGradleDependency extends GradleDependency {
+
+	public MethodGradleDependency(String configuration, String group,
+		String name, String version, String methodName) {
+
+		super(configuration, group, name, version);
+
+		_methodName = methodName;
+	}
+
+	public MethodGradleDependency(String configuration, String group,
+		String name, String version, int lineNumber, int lastLineNumber,
+		String methodName) {
+
+		super(configuration, group, name, version, lineNumber, lastLineNumber);
+
+		_methodName = methodName;
+	}
+
+	public String getMethodName() {
+		return _methodName;
+	}
+
+	private String _methodName;
+}
