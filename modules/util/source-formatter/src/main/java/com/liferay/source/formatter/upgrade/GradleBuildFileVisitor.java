479ff9ec0148711ec997e662b27079760e6fde59
Seiphon Wang <seiphon.wang@liferay.com>
2022-08-25 13:10:59 +0800

    LPS-148907 Improve GradleBuildFileVisitor to handle different kind of dependencies

--- modules/util/source-formatter/src/main/java/com/liferay/source/formatter/upgrade/GradleBuildFileVisitor.java
+++ modules/util/source-formatter/src/main/java/com/liferay/source/formatter/upgrade/GradleBuildFileVisitor.java
@@ -15,14 +15,21 @@
 package com.liferay.source.formatter.upgrade;
 
 import com.liferay.portal.kernel.util.StringUtil;
+import com.liferay.source.formatter.gradle.ExternalGradleDependency;
+import com.liferay.source.formatter.gradle.MethodGradleDependency;
+import com.liferay.source.formatter.gradle.NoTransitiveGradleDependency;
+import com.liferay.source.formatter.gradle.ProjectGradleDependency;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 
 import org.codehaus.groovy.ast.CodeVisitorSupport;
 import org.codehaus.groovy.ast.expr.ArgumentListExpression;
+import org.codehaus.groovy.ast.expr.ClosureExpression;
 import org.codehaus.groovy.ast.expr.ConstantExpression;
 import org.codehaus.groovy.ast.expr.Expression;
 import org.codehaus.groovy.ast.expr.MapEntryExpression;
@@ -57,8 +64,20 @@ public class GradleBuildFileVisitor extends CodeVisitorSupport {
 
 		List<Expression> expressions = argumentListExpression.getExpressions();
 
-		if ((expressions.size() == 1) &&
+		if (Objects.equals(_methodName, "project") &&
 			(expressions.get(0) instanceof ConstantExpression)) {
+
+			Expression argExpression = expressions.get(0);
+
+			String path = argExpression.getText();
+
+			GradleDependency gradleDependency = new ProjectGradleDependency(
+				_configuration, null, null, null, path, null);
+
+			_gradleDependencies.add(gradleDependency);
+		}
+		else if ((expressions.size() == 1) &&
+				 (expressions.get(0) instanceof ConstantExpression)) {
 
 			ConstantExpression constantExpression =
 				(ConstantExpression)expressions.get(0);
@@ -74,6 +93,30 @@ public class GradleBuildFileVisitor extends CodeVisitorSupport {
 
 				_gradleDependencies.add(gradleDependency);
 			}
+		}
+		else if ((expressions.size() == 1) &&
+				 (expressions.get(0) instanceof MethodCallExpression)) {
+
+			MethodCallExpression methodCallExpression =
+				(MethodCallExpression)expressions.get(0);
+
+			_methodName = methodCallExpression.getMethodAsString();
+
+			if (!_possibleMethodName.contains(_methodName)) {
+				GradleDependency gradleDependency = new MethodGradleDependency(
+					_configuration, null, null, null, _methodName);
+
+				_gradleDependencies.add(gradleDependency);
+			}
+
+			super.visitMethodCallExpression(methodCallExpression);
+
+			_methodName = null;
+		}
+		else if ((expressions.size() == 2) &&
+				 (expressions.get(1) instanceof ClosureExpression)) {
+
+			System.out.println(expressions.toString());
 		}
 
 		super.visitArgumentlistExpression(argumentListExpression);
@@ -102,6 +145,9 @@ public class GradleBuildFileVisitor extends CodeVisitorSupport {
 		Map<String, String> keyValues = new HashMap<>();
 
 		boolean gav = false;
+		boolean project = false;
+		boolean ext = false;
+		boolean withTranstive = false;
 
 		for (MapEntryExpression mapEntryExpression :
 				mapExpression.getMapEntryExpressions()) {
@@ -115,18 +161,56 @@ public class GradleBuildFileVisitor extends CodeVisitorSupport {
 
 			String value = valueExpression.getText();
 
+			if (StringUtil.equalsIgnoreCase(key, "ext")) {
+				ext = true;
+			}
+
 			if (StringUtil.equalsIgnoreCase(key, "group")) {
 				gav = true;
+			}
+
+			if (StringUtil.equalsIgnoreCase(key, "path")) {
+				project = true;
+			}
+
+			if (StringUtil.equalsIgnoreCase(key, "transitive")) {
+				withTranstive = true;
 			}
 
 			keyValues.put(key, value);
 		}
 
 		if (gav) {
-			GradleDependency gradleDependency = new GradleDependency(
-				_configuration, keyValues.get("group"), keyValues.get("name"),
-				keyValues.get("version"), _methodCallLineNumber,
-				_methodCallLastLineNumber);
+			if (withTranstive) {
+				GradleDependency gradleDependency =
+					new NoTransitiveGradleDependency(
+						_configuration, keyValues.get("group"),
+						keyValues.get("name"), keyValues.get("version"));
+
+				_gradleDependencies.add(gradleDependency);
+			}
+			else if (ext) {
+				GradleDependency gradleDependency =
+					new ExternalGradleDependency(
+						_configuration, keyValues.get("classifier"),
+						keyValues.get("ext"), keyValues.get("group"),
+						keyValues.get("name"), keyValues.get("version"));
+
+				_gradleDependencies.add(gradleDependency);
+			}
+			else {
+				GradleDependency gradleDependency = new GradleDependency(
+					_configuration, keyValues.get("group"),
+					keyValues.get("name"), keyValues.get("version"),
+					_methodCallLineNumber, _methodCallLastLineNumber);
+
+				_gradleDependencies.add(gradleDependency);
+			}
+		}
+		else if (project) {
+			GradleDependency gradleDependency = new ProjectGradleDependency(
+				_configuration, null, null, null, keyValues.get("path"),
+				keyValues.get("configuration"));
 
 			_gradleDependencies.add(gradleDependency);
 		}
@@ -165,6 +249,10 @@ public class GradleBuildFileVisitor extends CodeVisitorSupport {
 
 			_configuration = methodName;
 
+			if (_possibleMethodName.contains(_configuration)) {
+				return;
+			}
+
 			super.visitMethodCallExpression(methodCallExpression);
 
 			_configuration = null;
@@ -182,6 +270,9 @@ public class GradleBuildFileVisitor extends CodeVisitorSupport {
 	private boolean _inDependencies;
 	private int _methodCallLastLineNumber = -1;
 	private int _methodCallLineNumber = -1;
+	private String _methodName;
 	private int _numberOfBlocks;
+	private final List<String> _possibleMethodName = Arrays.asList(
+		"project", "files", "fileTree");
 
 }
