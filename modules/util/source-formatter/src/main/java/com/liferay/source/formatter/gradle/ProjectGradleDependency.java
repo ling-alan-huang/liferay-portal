8dbee9b9c7fd65a09b6f1f6d0adc3f668da5ff7a
Seiphon Wang <seiphon.wang@liferay.com>
2022-08-26 17:01:21 +0800

    LPS-148907 Support more kinds of ProjectGradleDependency

--- modules/util/source-formatter/src/main/java/com/liferay/source/formatter/gradle/ProjectGradleDependency.java
+++ modules/util/source-formatter/src/main/java/com/liferay/source/formatter/gradle/ProjectGradleDependency.java
@@ -14,6 +14,7 @@
 
 package com.liferay.source.formatter.gradle;
 
+import com.liferay.petra.string.StringBundler;
 import com.liferay.source.formatter.upgrade.GradleDependency;
 
 import java.text.MessageFormat;
@@ -28,22 +29,35 @@ public class ProjectGradleDependency extends GradleDependency {
 	public ProjectGradleDependency(
 		String configuration, String group, String name, String version,
 		int lineNumber, int lastLineNumber, String path,
-		String otherConfiguration) {
+		String otherConfiguration, String methodName, String methodArgs) {
 
 		super(configuration, group, name, version, lineNumber, lastLineNumber);
 
 		_path = path;
 		_otherConfiguration = otherConfiguration;
+		_methodName = methodName;
+		_methodArgs = methodArgs;
 	}
 
 	public ProjectGradleDependency(
 		String configuration, String group, String name, String version,
-		String path, String otherConfiguration) {
+		String path, String otherConfiguration, String methodName,
+		String methodArgs) {
 
 		super(configuration, group, name, version);
 
 		_path = path;
 		_otherConfiguration = otherConfiguration;
+		_methodName = methodName;
+		_methodArgs = methodArgs;
+	}
+
+	public String getMethodArgs() {
+		return _methodArgs;
+	}
+
+	public String getMethodName() {
+		return _methodName;
 	}
 
 	public String getOtherConfiguration() {
@@ -62,15 +76,39 @@ public class ProjectGradleDependency extends GradleDependency {
 	@Override
 	public String toString() {
 		if (_otherConfiguration == null) {
-			return MessageFormat.format(
-				"{0} project(\"{1}\")", getConfiguration(), _path);
+			StringBundler sb = new StringBundler(
+				MessageFormat.format(
+					"{0} project(\"{1}\")", getConfiguration(), _path));
+
+			if (_methodName != null) {
+				sb.append(".");
+				sb.append(_methodName);
+				sb.append("(\"");
+				sb.append(_methodArgs);
+				sb.append("\")");
+			}
+
+			return sb.toString();
 		}
 
-		return MessageFormat.format(
-			"{0} project(path: \"{1}\", configuration: \"{2}\")",
-			getConfiguration(), _path, _otherConfiguration);
+		StringBundler sb = new StringBundler(
+			MessageFormat.format(
+				"{0} project(path: \"{1}\", configuration: \"{2}\")",
+				getConfiguration(), _path, _otherConfiguration));
+
+		if (_methodName != null) {
+			sb.append(".");
+			sb.append(_methodName);
+			sb.append("(\"");
+			sb.append(_methodArgs);
+			sb.append("\")");
+		}
+
+		return sb.toString();
 	}
 
+	private final String _methodArgs;
+	private final String _methodName;
 	private final String _otherConfiguration;
 	private final String _path;
 
