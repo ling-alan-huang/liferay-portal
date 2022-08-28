13586fdced29bb2c801021afcdda96ff2000c437
Alan Huang <alan.huang@liferay.com>
2022-08-29 04:20:37 +0800

    LPS-160997 Rename package from 'internal.servlet.taglib.clay' to 'internal.frontend.taglib.clay.servlet.taglib'

--- modules/apps/depot/depot-web/src/main/java/com/liferay/depot/web/internal/frontend/taglib/clay/servlet/taglib/DepotDashboardApplicationNavigationCard.java
+++ modules/apps/depot/depot-web/src/main/java/com/liferay/depot/web/internal/frontend/taglib/clay/servlet/taglib/DepotDashboardApplicationNavigationCard.java
@@ -12,7 +12,7 @@
  * details.
  */
 
-package com.liferay.depot.web.internal.servlet.taglib.clay;
+package com.liferay.depot.web.internal.frontend.taglib.clay.servlet.taglib;
 
 import com.liferay.frontend.taglib.clay.servlet.taglib.NavigationCard;
 
