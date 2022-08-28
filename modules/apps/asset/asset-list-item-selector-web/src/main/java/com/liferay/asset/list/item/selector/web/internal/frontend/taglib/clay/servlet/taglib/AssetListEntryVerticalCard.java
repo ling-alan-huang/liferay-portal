18d7c4cfd7100f6008ae5043a18d7664b1228173
Alan Huang <alan.huang@liferay.com>
2022-08-29 04:03:17 +0800

    LPS-160997 Rename package from 'internal.servlet.taglib.clay' to 'internal.frontend.taglib.clay.servlet.taglib'

--- modules/apps/asset/asset-list-item-selector-web/src/main/java/com/liferay/asset/list/item/selector/web/internal/frontend/taglib/clay/servlet/taglib/AssetListEntryVerticalCard.java
+++ modules/apps/asset/asset-list-item-selector-web/src/main/java/com/liferay/asset/list/item/selector/web/internal/frontend/taglib/clay/servlet/taglib/AssetListEntryVerticalCard.java
@@ -12,7 +12,7 @@
  * details.
  */
 
-package com.liferay.asset.list.item.selector.web.internal.servlet.taglib.clay;
+package com.liferay.asset.list.item.selector.web.internal.frontend.taglib.clay.servlet.taglib;
 
 import com.liferay.asset.list.item.selector.web.internal.display.context.AssetListEntryItemSelectorDisplayContext;
 import com.liferay.asset.list.model.AssetListEntry;
