069548f8707540739e4852a0f35de9a629ed984c
Alan Huang <alan.huang@liferay.com>
2022-08-29 23:11:48 +0800

    LPS-160997 Rename package from 'internal.servlet.taglib.clay' to 'internal.frontend.taglib.clay.servlet.taglib'

--- modules/apps/site/site-browser-web/src/main/java/com/liferay/site/browser/web/internal/frontend/taglib/clay/servlet/taglib/SiteVerticalCard.java
+++ modules/apps/site/site-browser-web/src/main/java/com/liferay/site/browser/web/internal/frontend/taglib/clay/servlet/taglib/SiteVerticalCard.java
@@ -12,7 +12,7 @@
  * details.
  */
 
-package com.liferay.site.browser.web.internal.servlet.taglib.clay;
+package com.liferay.site.browser.web.internal.frontend.taglib.clay.servlet.taglib;
 
 import com.liferay.frontend.taglib.clay.servlet.taglib.VerticalCard;
 import com.liferay.petra.string.StringPool;
