802fa11c22aaa03995d4e2332ac04f605f5f3776
Alan Huang <alan.huang@liferay.com>
2022-08-29 13:12:11 +0800

    LPS-160997 Rename package from 'internal.servlet.taglib.clay' to 'internal.frontend.taglib.clay.servlet.taglib'

--- modules/apps/layout/layout-set-prototype-web/src/main/java/com/liferay/layout/set/prototype/web/internal/frontend/taglib/clay/servlet/taglib/LayoutSetPrototypeVerticalCard.java
+++ modules/apps/layout/layout-set-prototype-web/src/main/java/com/liferay/layout/set/prototype/web/internal/frontend/taglib/clay/servlet/taglib/LayoutSetPrototypeVerticalCard.java
@@ -12,7 +12,7 @@
  * details.
  */
 
-package com.liferay.layout.set.prototype.web.internal.servlet.taglib.clay;
+package com.liferay.layout.set.prototype.web.internal.frontend.taglib.clay.servlet.taglib;
 
 import com.liferay.frontend.taglib.clay.servlet.taglib.BaseVerticalCard;
 import com.liferay.frontend.taglib.clay.servlet.taglib.util.DropdownItem;
