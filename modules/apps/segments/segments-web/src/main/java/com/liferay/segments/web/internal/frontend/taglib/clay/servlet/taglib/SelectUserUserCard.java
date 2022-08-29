a2dbda0fcfda3a754a0e3f98d31f7852ac4725d8
Alan Huang <alan.huang@liferay.com>
2022-08-29 23:02:37 +0800

    LPS-160997 Rename package from 'internal.servlet.taglib.clay' to 'internal.frontend.taglib.clay.servlet.taglib'

--- modules/apps/segments/segments-web/src/main/java/com/liferay/segments/web/internal/frontend/taglib/clay/servlet/taglib/SelectUserUserCard.java
+++ modules/apps/segments/segments-web/src/main/java/com/liferay/segments/web/internal/frontend/taglib/clay/servlet/taglib/SelectUserUserCard.java
@@ -12,7 +12,7 @@
  * details.
  */
 
-package com.liferay.segments.web.internal.servlet.taglib.clay;
+package com.liferay.segments.web.internal.frontend.taglib.clay.servlet.taglib;
 
 import com.liferay.frontend.taglib.clay.servlet.taglib.BaseUserCard;
 import com.liferay.portal.kernel.dao.search.RowChecker;
