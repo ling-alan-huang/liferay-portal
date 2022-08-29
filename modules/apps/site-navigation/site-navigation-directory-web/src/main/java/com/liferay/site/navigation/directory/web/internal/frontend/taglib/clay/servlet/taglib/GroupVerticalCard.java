352352510f754fbfaf18d174ab156e902119a13b
Alan Huang <alan.huang@liferay.com>
2022-08-29 23:05:46 +0800

    LPS-160997 Rename package from 'internal.servlet.taglib.clay' to 'internal.frontend.taglib.clay.servlet.taglib'

--- modules/apps/site-navigation/site-navigation-directory-web/src/main/java/com/liferay/site/navigation/directory/web/internal/frontend/taglib/clay/servlet/taglib/GroupVerticalCard.java
+++ modules/apps/site-navigation/site-navigation-directory-web/src/main/java/com/liferay/site/navigation/directory/web/internal/frontend/taglib/clay/servlet/taglib/GroupVerticalCard.java
@@ -12,7 +12,7 @@
  * details.
  */
 
-package com.liferay.site.navigation.directory.web.internal.servlet.taglib.clay;
+package com.liferay.site.navigation.directory.web.internal.frontend.taglib.clay.servlet.taglib;
 
 import com.liferay.frontend.taglib.clay.servlet.taglib.VerticalCard;
 import com.liferay.portal.kernel.log.Log;
