57ed584ef868e9e34ef7f63272f2c0e564c8c87b
Alan Huang <alan.huang@liferay.com>
2022-08-29 23:20:35 +0800

    LPS-160997 Rename package from 'internal.servlet.taglib.clay' to 'internal.frontend.taglib.clay.servlet.taglib'

--- modules/apps/site/site-teams-web/src/main/java/com/liferay/site/teams/web/internal/frontend/taglib/clay/servlet/taglib/UserUserCard.java
+++ modules/apps/site/site-teams-web/src/main/java/com/liferay/site/teams/web/internal/frontend/taglib/clay/servlet/taglib/UserUserCard.java
@@ -12,7 +12,7 @@
  * details.
  */
 
-package com.liferay.site.teams.web.internal.servlet.taglib.clay;
+package com.liferay.site.teams.web.internal.frontend.taglib.clay.servlet.taglib;
 
 import com.liferay.frontend.taglib.clay.servlet.taglib.BaseUserCard;
 import com.liferay.frontend.taglib.clay.servlet.taglib.util.DropdownItem;
