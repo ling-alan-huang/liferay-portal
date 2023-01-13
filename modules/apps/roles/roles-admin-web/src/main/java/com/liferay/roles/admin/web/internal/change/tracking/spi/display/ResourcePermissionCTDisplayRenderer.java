4daeda561452ca7cef41ffda82b51a00f1ff674e
Alan Huang <alan.huang@liferay.com>
2023-01-13 14:54:01 +0800

    Revert "LPS-171976 Auto SF"
    This reverts commit dd98b3fea3996f00809024f66dfafe24e024b183.

--- modules/apps/roles/roles-admin-web/src/main/java/com/liferay/roles/admin/web/internal/change/tracking/spi/display/ResourcePermissionCTDisplayRenderer.java
+++ modules/apps/roles/roles-admin-web/src/main/java/com/liferay/roles/admin/web/internal/change/tracking/spi/display/ResourcePermissionCTDisplayRenderer.java
@@ -145,10 +145,11 @@ public class ResourcePermissionCTDisplayRenderer
 				locale, "x-permissions-for-x-x",
 				arguments.toArray(new String[0]), false);
 		}
-
-		return _language.format(
-			locale, "x-permissions-for-x-x-x", arguments.toArray(new String[0]),
-			false);
+		else {
+			return _language.format(
+				locale, "x-permissions-for-x-x-x",
+				arguments.toArray(new String[0]), false);
+		}
 	}
 
 	@Override
