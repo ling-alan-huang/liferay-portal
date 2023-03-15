7fc91f68f08caf9bdc128b288c65ed8dcd208141
Qi Zhang <qi.zhang@liferay.com>
2023-03-15 12:19:30 +0800

    LPS-178596 Move common to baseCheck

--- modules/util/source-formatter/src/main/java/com/liferay/source/formatter/check/BaseTagAttributesCheck.java
+++ modules/util/source-formatter/src/main/java/com/liferay/source/formatter/check/BaseTagAttributesCheck.java
@@ -17,6 +17,8 @@ package com.liferay.source.formatter.check;
 import com.liferay.petra.string.CharPool;
 import com.liferay.petra.string.StringBundler;
 import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.io.unsync.UnsyncBufferedReader;
+import com.liferay.portal.kernel.io.unsync.UnsyncStringReader;
 import com.liferay.portal.kernel.util.ListUtil;
 import com.liferay.portal.kernel.util.NaturalOrderStringComparator;
 import com.liferay.portal.kernel.util.StringUtil;
@@ -151,6 +153,58 @@ public abstract class BaseTagAttributesCheck extends BaseFileCheck {
 			if (!tag.equals(newTag)) {
 				return StringUtil.replace(content, tag, newTag);
 			}
+		}
+
+		return content;
+	}
+
+	protected String formatSingleLineTagAttributes(
+			String absolutePath, String content)
+		throws Exception {
+
+		StringBundler sb = new StringBundler();
+
+		try (UnsyncBufferedReader unsyncBufferedReader =
+				new UnsyncBufferedReader(new UnsyncStringReader(content))) {
+
+			String line = null;
+
+			while ((line = unsyncBufferedReader.readLine()) != null) {
+				String trimmedLine = StringUtil.trimLeading(line);
+
+				if (trimmedLine.matches("<\\w+ .*>.*")) {
+					String htmlTag = getTag(trimmedLine, 0);
+
+					if (htmlTag != null) {
+						String newHTMLTag = formatTagAttributes(
+							absolutePath, htmlTag, false, true);
+
+						line = StringUtil.replace(line, htmlTag, newHTMLTag);
+					}
+				}
+
+				for (String jspTag : getJSPTags(line)) {
+					boolean forceSingleLine = false;
+
+					if (!line.equals(jspTag)) {
+						forceSingleLine = true;
+					}
+
+					String newJSPTag = formatTagAttributes(
+						absolutePath, jspTag, false, forceSingleLine);
+
+					line = StringUtil.replace(line, jspTag, newJSPTag);
+				}
+
+				sb.append(line);
+				sb.append("\n");
+			}
+		}
+
+		content = sb.toString();
+
+		if (content.endsWith("\n")) {
+			content = content.substring(0, content.length() - 1);
 		}
 
 		return content;
--- modules/util/source-formatter/src/main/java/com/liferay/source/formatter/check/JellyTagAttributesCheck.java
+++ modules/util/source-formatter/src/main/java/com/liferay/source/formatter/check/JellyTagAttributesCheck.java
@@ -14,11 +14,6 @@
 
 package com.liferay.source.formatter.check;
 
-import com.liferay.petra.string.StringBundler;
-import com.liferay.portal.kernel.io.unsync.UnsyncBufferedReader;
-import com.liferay.portal.kernel.io.unsync.UnsyncStringReader;
-import com.liferay.portal.kernel.util.StringUtil;
-
 /**
  * @author Qi Zhang
  */
@@ -29,59 +24,7 @@ public class JellyTagAttributesCheck extends BaseTagAttributesCheck {
 			String fileName, String absolutePath, String content)
 		throws Exception {
 
-		return _formatSingleLineTagAttributes(absolutePath, content);
-	}
-
-	private String _formatSingleLineTagAttributes(
-			String absolutePath, String content)
-		throws Exception {
-
-		StringBundler sb = new StringBundler();
-
-		try (UnsyncBufferedReader unsyncBufferedReader =
-				new UnsyncBufferedReader(new UnsyncStringReader(content))) {
-
-			String line = null;
-
-			while ((line = unsyncBufferedReader.readLine()) != null) {
-				String trimmedLine = StringUtil.trimLeading(line);
-
-				if (trimmedLine.matches("<\\w+ .*>.*")) {
-					String htmlTag = getTag(trimmedLine, 0);
-
-					if (htmlTag != null) {
-						String newHTMLTag = formatTagAttributes(
-							absolutePath, htmlTag, false, true);
-
-						line = StringUtil.replace(line, htmlTag, newHTMLTag);
-					}
-				}
-
-				for (String jspTag : getJSPTags(line)) {
-					boolean forceSingleLine = false;
-
-					if (!line.equals(jspTag)) {
-						forceSingleLine = true;
-					}
-
-					String newJSPTag = formatTagAttributes(
-						absolutePath, jspTag, false, forceSingleLine);
-
-					line = StringUtil.replace(line, jspTag, newJSPTag);
-				}
-
-				sb.append(line);
-				sb.append("\n");
-			}
-		}
-
-		content = sb.toString();
-
-		if (content.endsWith("\n")) {
-			content = content.substring(0, content.length() - 1);
-		}
-
-		return content;
+		return formatSingleLineTagAttributes(absolutePath, content);
 	}
 
 }
