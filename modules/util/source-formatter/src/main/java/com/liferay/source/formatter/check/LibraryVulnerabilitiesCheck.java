fa6d284c0ed4082b83bc4edbe6d4d63549fd7106
Alan Huang <alan.huang@liferay.com>
2023-04-14 23:27:44 +0800

    LPS-150272 Avoid NPE

--- modules/util/source-formatter/src/main/java/com/liferay/source/formatter/check/LibraryVulnerabilitiesCheck.java
+++ modules/util/source-formatter/src/main/java/com/liferay/source/formatter/check/LibraryVulnerabilitiesCheck.java
@@ -622,8 +622,16 @@ public class LibraryVulnerabilitiesCheck extends BaseFileCheck {
 
 			JSONObject dataJSONObject = jsonObject.getJSONObject("data");
 
+			if (dataJSONObject == null) {
+				return Collections.emptyList();
+			}
+
 			JSONObject securityVulnerabilitiesJSONObject =
 				dataJSONObject.getJSONObject("securityVulnerabilities");
+
+			if (securityVulnerabilitiesJSONObject == null) {
+				return Collections.emptyList();
+			}
 
 			int totalCount = securityVulnerabilitiesJSONObject.getInt(
 				"totalCount");
