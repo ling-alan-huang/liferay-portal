bc6e335ecd0b5ae39d49d8a392e31e2eb5fd59a1
Qi Zhang <qi.zhang@liferay.com>
2023-04-24 10:20:47 +0800

    LPS-182475 Add logic check use 'toString' in StringBundler.concat

--- modules/util/source-formatter/src/main/java/com/liferay/source/formatter/checkstyle/check/StringCastCheck.java
+++ modules/util/source-formatter/src/main/java/com/liferay/source/formatter/checkstyle/check/StringCastCheck.java
@@ -14,7 +14,11 @@
 
 package com.liferay.source.formatter.checkstyle.check;
 
+import com.liferay.portal.kernel.util.ListUtil;
+import com.liferay.portal.kernel.util.StringUtil;
+
 import com.puppycrawl.tools.checkstyle.api.DetailAST;
+import com.puppycrawl.tools.checkstyle.api.FullIdent;
 import com.puppycrawl.tools.checkstyle.api.TokenTypes;
 
 import java.util.List;
@@ -45,9 +49,73 @@ public class StringCastCheck extends BaseCheck {
 				log(methodCallDetailAST, _MSG_UNNEEDED_STRING_CAST);
 			}
 		}
+
+		_checkUsePetraStringBundler(detailAST);
+	}
+
+	private void _checkUsePetraStringBundler(DetailAST detailAST) {
+		List<DetailAST> methodCallDetailASTs = getMethodCalls(
+			detailAST, "StringBundler", "concat");
+
+		if (ListUtil.isEmpty(methodCallDetailASTs)) {
+			return;
+		}
+
+		for (DetailAST methodCallDetailAST : methodCallDetailASTs) {
+			DetailAST eListDetailAST = methodCallDetailAST.findFirstToken(
+				TokenTypes.ELIST);
+
+			DetailAST childDetailAST = eListDetailAST.getFirstChild();
+
+			while (childDetailAST != null) {
+				if ((childDetailAST.getType() == TokenTypes.COMMA) ||
+					(childDetailAST.getType() != TokenTypes.EXPR)) {
+
+					childDetailAST = childDetailAST.getNextSibling();
+
+					continue;
+				}
+
+				DetailAST grandChildDetailAST = childDetailAST.getFirstChild();
+
+				if (grandChildDetailAST.getType() != TokenTypes.METHOD_CALL) {
+					childDetailAST = childDetailAST.getNextSibling();
+
+					continue;
+				}
+
+				DetailAST dotDetailAST = grandChildDetailAST.getFirstChild();
+
+				if (dotDetailAST.getType() != TokenTypes.DOT) {
+					childDetailAST = childDetailAST.getNextSibling();
+
+					continue;
+				}
+
+				FullIdent fullIdent = FullIdent.createFullIdent(dotDetailAST);
+
+				String fullIdentText = fullIdent.getText();
+
+				DetailAST childEListDetailAST =
+					grandChildDetailAST.findFirstToken(TokenTypes.ELIST);
+
+				if ((fullIdentText.endsWith("toString") &&
+					 (childEListDetailAST.getChildCount() == 0)) ||
+					(StringUtil.equals(fullIdentText, "String.valueOf") &&
+					 (childEListDetailAST.getChildCount() == 1))) {
+
+					log(grandChildDetailAST, _USE_PETRA_STRING_STRINGBUNDLER);
+				}
+
+				childDetailAST = childDetailAST.getNextSibling();
+			}
+		}
 	}
 
 	private static final String _MSG_UNNEEDED_STRING_CAST =
 		"string.cast.unneeded";
+
+	private static final String _USE_PETRA_STRING_STRINGBUNDLER =
+		"use.petra.string.string.bundler";
 
 }
