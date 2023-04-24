ba4faa0ce1cd7ab27f8a8ab3e84c59bd6738dfe2
Qi Zhang <qi.zhang@liferay.com>
2023-04-24 13:21:17 +0800

    LPS-182475 SF

--- modules/dxp/apps/saml/saml-opensaml-integration/src/main/java/com/liferay/saml/opensaml/integration/internal/servlet/profile/WebSsoProfileImpl.java
+++ modules/dxp/apps/saml/saml-opensaml-integration/src/main/java/com/liferay/saml/opensaml/integration/internal/servlet/profile/WebSsoProfileImpl.java
@@ -858,9 +858,8 @@ public class WebSsoProfileImpl extends BaseProfile implements WebSsoProfile {
 
 		throw new AssertionException(
 			StringBundler.concat(
-				"Date ", nowDateTime.toString(), " is before ",
-				lowerBoundDateTime.toString(), " including clock skew ",
-				clockSkew));
+				"Date ", nowDateTime, " is before ", lowerBoundDateTime,
+				" including clock skew ", clockSkew));
 	}
 
 	protected void verifyNotOnOrAfterDateTime(
@@ -877,9 +876,8 @@ public class WebSsoProfileImpl extends BaseProfile implements WebSsoProfile {
 
 		throw new ExpiredException(
 			StringBundler.concat(
-				"Date ", nowDateTime.toString(), " is after ",
-				upperBoundDateTime.toString(), " including clock skew ",
-				clockSkew));
+				"Date ", nowDateTime, " is after ", upperBoundDateTime,
+				" including clock skew ", clockSkew));
 	}
 
 	protected void verifyReplay(
