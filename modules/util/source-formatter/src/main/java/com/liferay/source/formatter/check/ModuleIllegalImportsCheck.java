440fab616163673c6269c372f5a68eb61a07f0e2
Alan Huang <alan.huang@liferay.com>
2023-05-07 23:00:02 +0800

    LPS-182475 Migrate SF check

--- modules/util/source-formatter/src/main/java/com/liferay/source/formatter/check/ModuleIllegalImportsCheck.java
+++ modules/util/source-formatter/src/main/java/com/liferay/source/formatter/check/ModuleIllegalImportsCheck.java
@@ -66,6 +66,12 @@ public class ModuleIllegalImportsCheck extends BaseFileCheck {
 					"modules, see LPS-77361");
 		}
 
+		if (content.contains("com.liferay.registry.Registry")) {
+			addMessage(
+				fileName,
+				"Do not use com.liferay.registry.Registry in modules");
+		}
+
 		if (content.contains("com.liferay.util.ContentUtil")) {
 			addMessage(
 				fileName,
