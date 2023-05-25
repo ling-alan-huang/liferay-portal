78a090a9af4193e0bdd84f9cbac22bd764f7ed83
Qi Zhang <qi.zhang@liferay.com>
2023-05-25 16:02:16 +0800

    LPS-185667 add testcase

--- modules/util/source-formatter/src/test/java/com/liferay/source/formatter/processor/JavaSourceProcessorTest.java
+++ modules/util/source-formatter/src/test/java/com/liferay/source/formatter/processor/JavaSourceProcessorTest.java
@@ -638,6 +638,15 @@ public class JavaSourceProcessorTest extends BaseSourceProcessorTestCase {
 	}
 
 	@Test
+	public void testPersistenceUpdate() throws Exception {
+		test(
+			"PersistenceUpdate.testjava",
+			"Update call should be reassigned to variable " +
+				"'layoutPrototypeLayout'",
+			36);
+	}
+
+	@Test
 	public void testProxyUsage() throws Exception {
 		test(
 			"ProxyUsage.testjava",
