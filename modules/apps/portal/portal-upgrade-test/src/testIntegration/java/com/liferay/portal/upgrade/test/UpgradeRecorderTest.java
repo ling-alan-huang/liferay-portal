5d6c06e40795d5308e97955fe06ce1e9147aa966
Qi Zhang <qi.zhang@liferay.com>
2023-06-13 10:32:58 +0800

    LPS-156857 SF Remove unnecessary empty line

--- modules/apps/portal/portal-upgrade-test/src/testIntegration/java/com/liferay/portal/upgrade/test/UpgradeRecorderTest.java
+++ modules/apps/portal/portal-upgrade-test/src/testIntegration/java/com/liferay/portal/upgrade/test/UpgradeRecorderTest.java
@@ -102,7 +102,6 @@ public class UpgradeRecorderTest {
 			release = _releaseLocalService.updateRelease(release);
 
 			StartupHelperUtil.setUpgrading(true);
-
 			StartupHelperUtil.setUpgrading(false);
 		}
 		finally {
@@ -147,7 +146,6 @@ public class UpgradeRecorderTest {
 	@Test
 	public void testSuccessByNoUpgrades() {
 		StartupHelperUtil.setUpgrading(true);
-
 		StartupHelperUtil.setUpgrading(false);
 
 		Assert.assertEquals("success", _getResult());
