23570c6ecc61c0e4313983e2181ec3e0161d1750
Qi Zhang <qi.zhang@liferay.com>
2023-08-04 13:56:47 +0800

    LPS-190915 source-formatter: don't need

--- modules/util/source-formatter/src/main/java/com/liferay/source/formatter/util/SourceFormatterUtil.java
+++ modules/util/source-formatter/src/main/java/com/liferay/source/formatter/util/SourceFormatterUtil.java
@@ -719,9 +719,7 @@ public class SourceFormatterUtil {
 						Arrays.asList("rev-parse", "--show-toplevel"), null,
 						null, false);
 
-					_gitTopLevelFolder = new File(
-						StringUtil.replace(
-							lines.get(0), CharPool.BACK_SLASH, CharPool.SLASH));
+					_gitTopLevelFolder = new File(lines.get(0));
 				}
 
 				List<String> gitFiles = new ArrayList<>();
