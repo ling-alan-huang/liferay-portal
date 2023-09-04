93fade912b2771c52a18509c3b03826c4dc4c010
Alan Huang <alan.huang@liferay.com>
2023-09-04 16:51:04 +0800

    LPS-177111 Simplify

--- modules/util/source-formatter/src/main/java/com/liferay/source/formatter/checkstyle/check/InstanceInitializerCheck.java
+++ modules/util/source-formatter/src/main/java/com/liferay/source/formatter/checkstyle/check/InstanceInitializerCheck.java
@@ -5,24 +5,26 @@
 
 package com.liferay.source.formatter.checkstyle.check;
 
-import com.liferay.petra.string.CharPool;
-import com.liferay.petra.string.StringBundler;
-import com.liferay.portal.kernel.util.ListUtil;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portal.kernel.util.Validator;
-import com.liferay.source.formatter.checkstyle.util.CheckstyleUtil;
+import com.liferay.source.formatter.check.util.BNDSourceUtil;
+import com.liferay.source.formatter.check.util.JavaSourceUtil;
+import com.liferay.source.formatter.check.util.SourceUtil;
+import com.liferay.source.formatter.parser.JavaClass;
+import com.liferay.source.formatter.parser.JavaClassParser;
+import com.liferay.source.formatter.parser.JavaTerm;
 import com.liferay.source.formatter.util.FileUtil;
 
-import com.puppycrawl.tools.checkstyle.JavaParser;
 import com.puppycrawl.tools.checkstyle.api.DetailAST;
-import com.puppycrawl.tools.checkstyle.api.FileContents;
-import com.puppycrawl.tools.checkstyle.api.FileText;
 import com.puppycrawl.tools.checkstyle.api.FullIdent;
 import com.puppycrawl.tools.checkstyle.api.TokenTypes;
 
 import java.io.File;
 
 import java.util.List;
+import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -50,8 +52,6 @@ public class InstanceInitializerCheck extends BaseCheck {
 			return;
 		}
 
-		_checkVariableAssign(detailAST, parentDetailAST);
-
 		DetailAST childDetailAST = detailAST.getFirstChild();
 
 		if (childDetailAST.getType() != TokenTypes.SLIST) {
@@ -60,6 +60,9 @@ public class InstanceInitializerCheck extends BaseCheck {
 
 		List<DetailAST> exprDetailASTList = getAllChildTokens(
 			childDetailAST, false, TokenTypes.EXPR);
+
+		_checkVariableAssign(
+			detailAST, exprDetailASTList, parentDetailAST, getAbsolutePath());
 
 		if (exprDetailASTList.size() < 2) {
 			return;
@@ -128,258 +131,9 @@ public class InstanceInitializerCheck extends BaseCheck {
 		}
 	}
 
-	private void _checkIfStatement(
-		DetailAST detailAST, DetailAST classDefDetailAST,
-		DetailAST instanceInitDetailAST) {
-
-		DetailAST sListDetailAST = detailAST.findFirstToken(TokenTypes.SLIST);
-
-		DetailAST childDetailAST = sListDetailAST.getFirstChild();
-
-		DetailAST assignDetailAST = null;
-
-		while (childDetailAST != null) {
-			int tokenType = childDetailAST.getType();
-
-			if ((tokenType == TokenTypes.SEMI) ||
-				(tokenType == TokenTypes.RCURLY)) {
-
-				childDetailAST = childDetailAST.getNextSibling();
-
-				continue;
-			}
-
-			if (tokenType != TokenTypes.EXPR) {
-				return;
-			}
-
-			assignDetailAST = childDetailAST.getFirstChild();
-
-			break;
-		}
-
-		if ((assignDetailAST == null) ||
-			(assignDetailAST.getType() != TokenTypes.ASSIGN)) {
-
-			return;
-		}
-
-		String assignName = getName(assignDetailAST);
-
-		if (_checkIsLocalVariable(
-				assignDetailAST, getStartLineNumber(instanceInitDetailAST),
-				assignName) ||
-			!_checkSameAssign(sListDetailAST, assignName)) {
-
-			return;
-		}
-
-		if (_checkSupportsFunction(classDefDetailAST, assignName)) {
-			log(
-				detailAST, _MSG_SIMPLY_BY_CALL_LAMBDA,
-				getStartLineNumber(detailAST), getEndLineNumber(detailAST),
-				StringUtil.upperCaseFirstLetter(assignName));
-		}
-	}
-
-	private boolean _checkIsLocalVariable(
-		DetailAST assignDetailAST, int startLineNumber, String variableName) {
-
-		DetailAST previousDetailAST = assignDetailAST.getParent();
-
-		while (true) {
-			if (previousDetailAST.getType() == TokenTypes.INSTANCE_INIT) {
-				if (getStartLineNumber(previousDetailAST) == startLineNumber) {
-					return false;
-				}
-
-				return true;
-			}
-
-			if ((previousDetailAST.getType() == TokenTypes.VARIABLE_DEF) &&
-				StringUtil.equals(getName(previousDetailAST), variableName)) {
-
-				return true;
-			}
-
-			DetailAST previousSiblingDetailAST =
-				previousDetailAST.getPreviousSibling();
-
-			if (previousSiblingDetailAST != null) {
-				previousDetailAST = previousSiblingDetailAST;
-
-				continue;
-			}
-
-			previousDetailAST = previousDetailAST.getParent();
-		}
-	}
-
-	private void _checkMethodCall(
-		DetailAST detailAST, DetailAST classDefDetailAST) {
-
-		String methodName = getMethodName(detailAST);
-
-		if (!methodName.matches("set[A-Z]\\w+")) {
-			return;
-		}
-
-		DetailAST eListDetailAST = detailAST.findFirstToken(TokenTypes.ELIST);
-
-		if (eListDetailAST.getChildCount() > 1) {
-			return;
-		}
-
-		DetailAST eListChildDetailAST = eListDetailAST.getFirstChild();
-
-		if (eListChildDetailAST == null) {
-			return;
-		}
-
-		if (eListChildDetailAST.getType() == TokenTypes.LAMBDA) {
-			return;
-		}
-		else if (eListChildDetailAST.getType() == TokenTypes.EXPR) {
-			DetailAST firstChildDetailAST = eListChildDetailAST.getFirstChild();
-
-			if ((firstChildDetailAST == null) ||
-				(firstChildDetailAST.getType() == TokenTypes.METHOD_REF)) {
-
-				return;
-			}
-		}
-
-		String variableName = StringUtil.lowerCaseFirstLetter(
-			methodName.substring(3));
-
-		if (_checkVariableDirectAssignment(
-				variableName, classDefDetailAST, methodName)) {
-
-			log(detailAST, _MSG_USE_ASSIGN_INSTEAD, variableName, methodName);
-		}
-	}
-
-	private boolean _checkSameAssign(DetailAST detailAST, String assignName) {
-		if (Validator.isNull(assignName)) {
-			return false;
-		}
-
-		while (detailAST != null) {
-			if (detailAST.getType() == TokenTypes.SLIST) {
-				break;
-			}
-
-			DetailAST firstChilddetailAST = detailAST.findFirstToken(
-				TokenTypes.SLIST);
-
-			if (firstChilddetailAST == null) {
-				detailAST = detailAST.getFirstChild();
-			}
-			else {
-				detailAST = firstChilddetailAST;
-			}
-		}
-
-		if (detailAST == null) {
-			return false;
-		}
-
-		DetailAST childDetailAST = detailAST.getFirstChild();
-
-		int childCount = 0;
-
-		while (childDetailAST != null) {
-			if ((childDetailAST.getType() == TokenTypes.SEMI) ||
-				(childDetailAST.getType() == TokenTypes.RCURLY)) {
-
-				childDetailAST = childDetailAST.getNextSibling();
-
-				continue;
-			}
-
-			childCount++;
-
-			if ((childCount > 1) ||
-				(childDetailAST.getType() != TokenTypes.EXPR)) {
-
-				return false;
-			}
-
-			DetailAST exprChildDetailAST = childDetailAST.getFirstChild();
-
-			if (exprChildDetailAST.getType() != TokenTypes.ASSIGN) {
-				return false;
-			}
-
-			String variableName = getName(exprChildDetailAST);
-
-			if (!StringUtil.equals(variableName, assignName)) {
-				return false;
-			}
-
-			childDetailAST = childDetailAST.getNextSibling();
-		}
-
-		detailAST = detailAST.getNextSibling();
-
-		if (detailAST != null) {
-			return _checkSameAssign(detailAST, assignName);
-		}
-
-		return true;
-	}
-
-	private boolean _checkSupportsFunction(
-		DetailAST detailAST, String variableName) {
-
-		List<DetailAST> methodDefDetailASTs = getAllChildTokens(
-			detailAST, true, TokenTypes.METHOD_DEF);
-
-		for (DetailAST methodDefDetailAST : methodDefDetailASTs) {
-			String methodName = getName(methodDefDetailAST);
-
-			if (!StringUtil.equals(
-					"set" + StringUtil.upperCaseFirstLetter(variableName),
-					methodName)) {
-
-				continue;
-			}
-
-			DetailAST modifiersDetailAST = methodDefDetailAST.findFirstToken(
-				TokenTypes.MODIFIERS);
-
-			DetailAST modifiersFirstChildDetailAST =
-				modifiersDetailAST.getLastChild();
-
-			if ((modifiersFirstChildDetailAST != null) &&
-				(modifiersFirstChildDetailAST.getType() ==
-					TokenTypes.LITERAL_PRIVATE)) {
-
-				continue;
-			}
-
-			List<DetailAST> parameterDefDetailASTs = getParameterDefs(
-				methodDefDetailAST);
-
-			if (ListUtil.isEmpty(parameterDefDetailASTs) ||
-				(parameterDefDetailASTs.size() > 1)) {
-
-				continue;
-			}
-
-			String parameterType = getTypeName(
-				parameterDefDetailASTs.get(0), false);
-
-			if (StringUtil.equals(parameterType, "UnsafeSupplier")) {
-				return true;
-			}
-		}
-
-		return false;
-	}
-
 	private void _checkVariableAssign(
-		DetailAST detailAST, DetailAST parentDetailAST) {
+		DetailAST detailAST, List<DetailAST> exprDetailASTList,
+		DetailAST parentDetailAST, String absolutePath) {
 
 		DetailAST identDetailAST = parentDetailAST.findFirstToken(
 			TokenTypes.IDENT);
@@ -425,164 +179,86 @@ public class InstanceInitializerCheck extends BaseCheck {
 			return;
 		}
 
-		DetailAST rootDetailAST = _getRootDetailAST(className);
+		File javaFile = JavaSourceUtil.getJavaFile(
+			getFullyQualifiedTypeName(
+				identDetailAST.getText(), detailAST, false),
+			_getRootDirName(absolutePath),
+			_getBundleSymbolicNamesMap(absolutePath));
 
-		if ((rootDetailAST == null) ||
-			!StringUtil.equals(packageName, getPackageName(rootDetailAST))) {
-
+		if (javaFile == null) {
 			return;
 		}
 
-		DetailAST slistDetailAST = detailAST.findFirstToken(TokenTypes.SLIST);
+		JavaClass javaClass = null;
 
-		DetailAST classDefDetailAST = _getClassDefDetailAST(rootDetailAST);
+		try {
+			javaClass = JavaClassParser.parseJavaClass(
+				SourceUtil.getAbsolutePath(javaFile), FileUtil.read(javaFile));
 
-		DetailAST childDetailAST = slistDetailAST.getFirstChild();
+			if (javaClass == null) {
+				return;
+			}
+		}
+		catch (Exception exception) {
+			if (_log.isDebugEnabled()) {
+				_log.debug(exception);
+			}
+		}
 
-		while (childDetailAST != null) {
-			int tokenType = childDetailAST.getType();
+		for (DetailAST exprDetailAST : exprDetailASTList) {
+			DetailAST childDetailAST = exprDetailAST.getFirstChild();
 
-			if ((tokenType == TokenTypes.SEMI) ||
-				(tokenType == TokenTypes.RCURLY)) {
-
-				childDetailAST = childDetailAST.getNextSibling();
-
+			if (childDetailAST.getType() != TokenTypes.METHOD_CALL) {
 				continue;
 			}
 
-			if (tokenType == TokenTypes.EXPR) {
-				DetailAST grandChildDetailAST = childDetailAST.getFirstChild();
+			String methodName = getMethodName(childDetailAST);
 
-				if (grandChildDetailAST.getType() == TokenTypes.METHOD_CALL) {
-					_checkMethodCall(grandChildDetailAST, classDefDetailAST);
+			if (!methodName.matches("set[A-Z]\\w*")) {
+				continue;
+			}
+
+			String variableName = StringUtil.lowerCaseFirstLetter(
+				methodName.substring(3));
+
+			Pattern pattern = Pattern.compile(
+				"\\s(\\S+)\\s+(\\S+\\.)?" + variableName);
+
+			for (JavaTerm javaTerm : javaClass.getChildJavaTerms()) {
+				if (!javaTerm.isJavaVariable() || javaTerm.isPrivate()) {
+					continue;
+				}
+
+				Matcher matcher = pattern.matcher(javaTerm.getContent());
+
+				if (matcher.find()) {
+					log(
+						detailAST, _MSG_USE_ASSIGN_INSTEAD, javaTerm.getName(),
+						methodName);
 				}
 			}
-
-			if (tokenType == TokenTypes.LITERAL_IF) {
-				_checkIfStatement(childDetailAST, classDefDetailAST, detailAST);
-			}
-
-			childDetailAST = childDetailAST.getNextSibling();
 		}
 	}
 
-	private boolean _checkVariableDirectAssignment(
-		String variableName, DetailAST detailAST, String methodName) {
+	private synchronized Map<String, String> _getBundleSymbolicNamesMap(
+		String absolutePath) {
 
-		DetailAST objBlockDetailAST = detailAST.findFirstToken(
-			TokenTypes.OBJBLOCK);
-
-		List<DetailAST> methodDefinitionDetailASTs = getAllChildTokens(
-			objBlockDetailAST, false, TokenTypes.METHOD_DEF);
-
-		boolean containMethod = false;
-
-		for (DetailAST methodDefinitionDetailAST : methodDefinitionDetailASTs) {
-			if (StringUtil.equals(
-					getName(methodDefinitionDetailAST), methodName)) {
-
-				containMethod = true;
-
-				break;
-			}
+		if (_bundleSymbolicNamesMap == null) {
+			_bundleSymbolicNamesMap = BNDSourceUtil.getBundleSymbolicNamesMap(
+				_getRootDirName(absolutePath));
 		}
 
-		if (!containMethod) {
-			return false;
-		}
-
-		DetailAST variableDefinitionDetailAST = getVariableDefinitionDetailAST(
-			detailAST, variableName);
-
-		if (variableDefinitionDetailAST == null) {
-			return false;
-		}
-
-		DetailAST modifiersDetailAST =
-			variableDefinitionDetailAST.findFirstToken(TokenTypes.MODIFIERS);
-
-		DetailAST modifiersFirstChildDetailAST =
-			modifiersDetailAST.getLastChild();
-
-		if ((modifiersFirstChildDetailAST != null) &&
-			(modifiersFirstChildDetailAST.getType() ==
-				TokenTypes.LITERAL_PRIVATE)) {
-
-			return false;
-		}
-
-		return true;
+		return _bundleSymbolicNamesMap;
 	}
 
-	private DetailAST _getClassDefDetailAST(DetailAST rootDetailAST) {
-		DetailAST nextDetailAST = rootDetailAST.getNextSibling();
-
-		while (nextDetailAST != null) {
-			if (nextDetailAST.getType() == TokenTypes.CLASS_DEF) {
-				return nextDetailAST;
-			}
-
-			nextDetailAST = nextDetailAST.getNextSibling();
+	private synchronized String _getRootDirName(String absolutePath) {
+		if (_rootDirName != null) {
+			return _rootDirName;
 		}
 
-		return null;
-	}
+		_rootDirName = SourceUtil.getRootDirName(absolutePath);
 
-	private String _getClassPath(String className) {
-		String absolutePath = StringUtil.replace(
-			getAbsolutePath(), CharPool.BACK_SLASH, CharPool.SLASH);
-
-		int pos = absolutePath.lastIndexOf("/src/main");
-
-		if (pos == -1) {
-			return null;
-		}
-
-		absolutePath = absolutePath.substring(0, pos);
-
-		if (!absolutePath.endsWith("-api") &&
-			!absolutePath.endsWith("-client") &&
-			!absolutePath.endsWith("-impl") &&
-			!absolutePath.endsWith("-service") &&
-			!absolutePath.endsWith("-test")) {
-
-			return null;
-		}
-
-		pos = absolutePath.lastIndexOf("-");
-
-		return StringBundler.concat(
-			absolutePath.substring(0, pos + 1), "api/src/main/java/",
-			StringUtil.replace(className, CharPool.PERIOD, CharPool.SLASH),
-			".java");
-	}
-
-	private DetailAST _getRootDetailAST(String className) {
-		String classPath = _getClassPath(className);
-
-		if (Validator.isNull(classPath)) {
-			return null;
-		}
-
-		File file = new File(classPath);
-
-		if (!file.exists()) {
-			return null;
-		}
-
-		try {
-			String content = FileUtil.read(file);
-
-			FileText fileText = new FileText(
-				file, CheckstyleUtil.getLines(content));
-
-			FileContents fileContents = new FileContents(fileText);
-
-			return JavaParser.parse(fileContents);
-		}
-		catch (Exception exception) {
-			return null;
-		}
+		return _rootDirName;
 	}
 
 	private boolean _isSameAppDTO(String packageName, String className) {
@@ -609,7 +285,13 @@ public class InstanceInitializerCheck extends BaseCheck {
 
 	private static final String _MSG_USE_ASSIGN_INSTEAD = "assign.use.instead";
 
+	private static final Log _log = LogFactoryUtil.getLog(
+		InstanceInitializerCheck.class);
+
 	private static final Pattern _appRootNamePattern = Pattern.compile(
 		"com\\.liferay\\.\\w+\\.");
+
+	private volatile Map<String, String> _bundleSymbolicNamesMap;
+	private volatile String _rootDirName;
 
 }
