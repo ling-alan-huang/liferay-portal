d4c701ee9a5a3854bc8835c03d000c7e30c411e3
Alan Huang <alan.huang@liferay.com>
2023-09-06 15:12:00 +0800

    LPS-177111 SF, use set calls

--- modules/apps/object/object-admin-rest-impl/src/main/java/com/liferay/object/admin/rest/internal/dto/v1_0/converter/ObjectValidationRuleDTOConverter.java
+++ modules/apps/object/object-admin-rest-impl/src/main/java/com/liferay/object/admin/rest/internal/dto/v1_0/converter/ObjectValidationRuleDTOConverter.java
@@ -78,25 +78,39 @@ public class ObjectValidationRuleDTOConverter
 					objectDefinition.getExternalReferenceCode();
 				objectDefinitionId =
 					serviceBuilderObjectValidationRule.getObjectDefinitionId();
+				script = serviceBuilderObjectValidationRule.getScript();
 
-				if (FeatureFlagManagerUtil.isEnabled("LPS-187846")) {
-					objectValidationRuleSettings =
-						TransformUtil.transformToArray(
+				setObjectValidationRuleSettings(
+					() -> {
+						if (!FeatureFlagManagerUtil.isEnabled("LPS-187846")) {
+							return null;
+						}
+
+						return TransformUtil.transformToArray(
 							serviceBuilderObjectValidationRule.
 								getObjectValidationRuleSettings(),
 							objectValidationRuleSetting ->
 								_toObjectValidationRuleSetting(
 									objectValidationRuleSetting),
 							ObjectValidationRuleSetting.class);
-					outputType = ObjectValidationRule.OutputType.create(
-						serviceBuilderObjectValidationRule.getOutputType());
-				}
+					});
+				setOutputType(
+					() -> {
+						if (!FeatureFlagManagerUtil.isEnabled("LPS-187846")) {
+							return null;
+						}
 
-				script = serviceBuilderObjectValidationRule.getScript();
+						return ObjectValidationRule.OutputType.create(
+							serviceBuilderObjectValidationRule.getOutputType());
+					});
+				setSystem(
+					() -> {
+						if (!FeatureFlagManagerUtil.isEnabled("LPS-193355")) {
+							return null;
+						}
 
-				if (FeatureFlagManagerUtil.isEnabled("LPS-193355")) {
-					system = serviceBuilderObjectValidationRule.getSystem();
-				}
+						return serviceBuilderObjectValidationRule.getSystem();
+					});
 			}
 		};
 	}
