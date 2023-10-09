e16300e3d05e53c5c6f8d03f572d2fe94907778c
Alan Huang <alan.huang@liferay.com>
2023-10-09 13:48:14 +0800

    LPS-197107 File should come before folder

--- modules/util/source-formatter/src/main/java/com/liferay/source/formatter/check/comparator/PropertyValueComparator.java
+++ modules/util/source-formatter/src/main/java/com/liferay/source/formatter/check/comparator/PropertyValueComparator.java
@@ -6,6 +6,7 @@
 package com.liferay.source.formatter.check.comparator;
 
 import com.liferay.petra.string.CharPool;
+import com.liferay.petra.string.StringPool;
 import com.liferay.portal.kernel.util.ArrayUtil;
 import com.liferay.portal.kernel.util.NaturalOrderStringComparator;
 import com.liferay.portal.kernel.util.StringUtil;
@@ -42,6 +43,31 @@ public class PropertyValueComparator extends NaturalOrderStringComparator {
 			}
 			else if (Character.isLowerCase(c1) && Character.isUpperCase(c2)) {
 				return 1;
+			}
+		}
+
+		if ((x > 0) && (s1.charAt(x - 1) == CharPool.SLASH)) {
+			if (c1 == CharPool.STAR) {
+				return -1;
+			}
+
+			if (c2 == CharPool.STAR) {
+				return 1;
+			}
+
+			if ((s1.indexOf(StringPool.PERIOD, x) != -1) &&
+				(s2.indexOf(StringPool.PERIOD, x) != -1)) {
+
+				if ((s1.indexOf(StringPool.SLASH, x) == -1) &&
+					(s2.indexOf(StringPool.SLASH, x) != -1)) {
+
+					return -1;
+				}
+				else if ((s1.indexOf(StringPool.SLASH, x) != -1) &&
+						 (s2.indexOf(StringPool.SLASH, x) == -1)) {
+
+					return 1;
+				}
 			}
 		}
 
