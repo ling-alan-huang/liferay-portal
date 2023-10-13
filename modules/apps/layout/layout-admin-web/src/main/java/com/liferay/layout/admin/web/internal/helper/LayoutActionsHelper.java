921c5f412a0af80e6589c922d23d0fa668aece22
Alan Huang <alan.huang@liferay.com>
2023-10-13 22:34:18 +0800

    LPS-196539 Rename

--- modules/apps/layout/layout-admin-web/src/main/java/com/liferay/layout/admin/web/internal/helper/LayoutActionsHelper.java
+++ modules/apps/layout/layout-admin-web/src/main/java/com/liferay/layout/admin/web/internal/helper/LayoutActionsHelper.java
@@ -306,13 +306,13 @@ public class LayoutActionsHelper {
 				explicitlyAddedPortlet.getPortletId());
 		}
 
-		List<PortletPreferences> portletPreferences =
+		List<PortletPreferences> portletPreferencesList =
 			PortletPreferencesLocalServiceUtil.getPortletPreferences(
 				PortletKeys.PREFS_OWNER_ID_DEFAULT,
 				PortletKeys.PREFS_OWNER_TYPE_LAYOUT, layout.getPlid());
 
-		for (PortletPreferences curPortletPreferences : portletPreferences) {
-			String portletId = curPortletPreferences.getPortletId();
+		for (PortletPreferences portletPreferences : portletPreferencesList) {
+			String portletId = portletPreferences.getPortletId();
 
 			Portlet portlet = PortletLocalServiceUtil.getPortletById(
 				layout.getCompanyId(), portletId);
