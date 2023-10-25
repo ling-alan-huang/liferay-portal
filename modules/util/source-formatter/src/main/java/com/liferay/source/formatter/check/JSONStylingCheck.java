52d9b0a17304aaaae331064eb5cd2d5065800f0d
Alan Huang <alan.huang@liferay.com>
2023-10-25 22:19:38 +0800

    LPS-199559 Fix indentation in #cdata-value

--- modules/util/source-formatter/src/main/java/com/liferay/source/formatter/check/JSONStylingCheck.java
+++ modules/util/source-formatter/src/main/java/com/liferay/source/formatter/check/JSONStylingCheck.java
@@ -5,6 +5,8 @@
 
 package com.liferay.source.formatter.check;
 
+import com.liferay.petra.string.CharPool;
+import com.liferay.petra.string.StringBundler;
 import com.liferay.petra.string.StringPool;
 import com.liferay.portal.json.JSONArrayImpl;
 import com.liferay.portal.json.JSONObjectImpl;
@@ -14,6 +16,7 @@ import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portal.kernel.util.Validator;
+import com.liferay.source.formatter.check.util.SourceUtil;
 
 /**
  * @author Hugo Huijser
@@ -22,7 +25,8 @@ public class JSONStylingCheck extends BaseFileCheck {
 
 	@Override
 	protected String doProcess(
-		String fileName, String absolutePath, String content) {
+			String fileName, String absolutePath, String content)
+		throws JSONException {
 
 		if (Validator.isNull(content)) {
 			return StringPool.BLANK;
@@ -51,7 +55,143 @@ public class JSONStylingCheck extends BaseFileCheck {
 			return content;
 		}
 
-		return content;
+		return _formatQuotedJSON(content, "#cdata-value");
+	}
+
+	private String _fixIndentation(String content, String indent) {
+		String[] lines = content.split("\n");
+
+		StringBundler sb = new StringBundler(lines.length * 2);
+
+		for (String line : lines) {
+			String trimmedLine = StringUtil.trimLeading(line);
+
+			if (!trimmedLine.equals("[") && !trimmedLine.equals("]")) {
+				if (!trimmedLine.startsWith("\"")) {
+					return content;
+				}
+
+				trimmedLine = StringUtil.trimLeading(trimmedLine.substring(1));
+
+				if (trimmedLine.endsWith("\"")) {
+					trimmedLine = StringUtil.replaceLast(trimmedLine, "\"", "");
+				}
+				else if (trimmedLine.endsWith("\",")) {
+					trimmedLine = StringUtil.replaceLast(
+						trimmedLine, "\",", "");
+				}
+
+				trimmedLine = trimmedLine.replaceAll("\\\\\"", "\"");
+
+				if (Validator.isNull(trimmedLine)) {
+					continue;
+				}
+			}
+
+			sb.append(trimmedLine);
+			sb.append("\n");
+		}
+
+		if (sb.index() > 0) {
+			sb.setIndex(sb.index() - 1);
+		}
+
+		String replacement = StringPool.BLANK;
+
+		try {
+			replacement = JSONUtil.toString(new JSONArrayImpl(sb.toString()));
+		}
+		catch (JSONException jsonException) {
+			if (_log.isDebugEnabled()) {
+				_log.debug(jsonException);
+			}
+
+			return content;
+		}
+
+		lines = replacement.split("\n");
+
+		sb = new StringBundler((lines.length * 5) + 3);
+
+		sb.append(StringPool.OPEN_BRACKET);
+		sb.append("\n");
+
+		for (int i = 1; i < (lines.length - 1); i++) {
+			String line = lines[i];
+
+			if (line.startsWith(StringPool.TAB)) {
+				line = line.substring(1);
+			}
+
+			line = StringUtil.replace(line, "\"", "\\\"");
+			line = StringUtil.replace(
+				line, CharPool.TAB, StringPool.FOUR_SPACES);
+
+			sb.append(indent);
+			sb.append(StringPool.TAB);
+			sb.append(StringPool.QUOTE + line + StringPool.QUOTE);
+			sb.append(StringPool.COMMA);
+			sb.append("\n");
+		}
+
+		sb.setIndex(sb.index() - 2);
+
+		sb.append("\n");
+		sb.append(indent);
+		sb.append(StringPool.CLOSE_BRACKET);
+
+		return sb.toString();
+	}
+
+	private String _formatQuotedJSON(String content, String key)
+		throws JSONException {
+
+		key = StringUtil.quote(key, StringPool.QUOTE) + ": [";
+
+		int x = -1;
+
+		while (true) {
+			x = content.indexOf(key, x + 1);
+
+			if (x == -1) {
+				return content;
+			}
+
+			int y = x;
+
+			while (true) {
+				y = content.indexOf("]", y + 1);
+
+				if (y == -1) {
+					continue;
+				}
+
+				String line = getLine(content, getLineNumber(content, y));
+
+				String trimmedLine = line.trim();
+
+				if (trimmedLine.equals("]") || trimmedLine.equals("],")) {
+					String quotedJSON = content.substring(
+						content.indexOf("[", x), y + 1);
+
+					String newQuotedJSON = _fixIndentation(
+						quotedJSON,
+						SourceUtil.getIndent(
+							getLine(content, getLineNumber(content, x))));
+
+					if (quotedJSON.equals(newQuotedJSON)) {
+						break;
+					}
+
+					return StringUtil.replaceFirst(
+						content, quotedJSON, newQuotedJSON, x);
+				}
+
+				y = y + 1;
+			}
+
+			x = x + 1;
+		}
 	}
 
 	private static final Log _log = LogFactoryUtil.getLog(
