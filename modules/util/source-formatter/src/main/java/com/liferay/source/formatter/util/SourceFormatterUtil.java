88e419d9e115dc6e136f8c94e61a1a66f21070c3
Qi Zhang <qi.zhang@liferay.com>
2023-12-04 15:35:03 +0800

    Revert "LPS-196218 source-formatter: fixes the NPE in the Poshi source processor"
    This reverts commit 0aee2b85191df3ee8ac0077ec29b89e6c0d486c0.

--- modules/util/source-formatter/src/main/java/com/liferay/source/formatter/util/SourceFormatterUtil.java
+++ modules/util/source-formatter/src/main/java/com/liferay/source/formatter/util/SourceFormatterUtil.java
@@ -201,10 +201,6 @@ public class SourceFormatterUtil {
 	}
 
 	public static File getFile(String baseDirName, String fileName, int level) {
-		if (Validator.isBlank(baseDirName)) {
-			baseDirName = "./";
-		}
-
 		for (int i = 0; i < level; i++) {
 			File file = new File(baseDirName + fileName);
 
