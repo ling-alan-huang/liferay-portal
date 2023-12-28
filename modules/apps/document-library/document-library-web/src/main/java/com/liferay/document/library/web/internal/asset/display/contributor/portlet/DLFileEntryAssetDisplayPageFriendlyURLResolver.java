9e0360db0c07bb467094addf127d87dda35a4476
Alan Huang <alan.huang@liferay.com>
2023-12-28 17:51:59 +0800

    LPS-203765 Move package, the package should have asset

--- modules/apps/document-library/document-library-web/src/main/java/com/liferay/document/library/web/internal/asset/display/contributor/portlet/DLFileEntryAssetDisplayPageFriendlyURLResolver.java
+++ modules/apps/document-library/document-library-web/src/main/java/com/liferay/document/library/web/internal/asset/display/contributor/portlet/DLFileEntryAssetDisplayPageFriendlyURLResolver.java
@@ -3,7 +3,7 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-package com.liferay.document.library.web.internal.info.display.contributor.portlet;
+package com.liferay.document.library.web.internal.asset.display.contributor.portlet;
 
 import com.liferay.asset.display.page.portlet.BaseAssetDisplayPageFriendlyURLResolver;
 import com.liferay.portal.kernel.portlet.FriendlyURLResolver;
