39c34dbc8231964e02e4830f008231091a690f0d
Alan Huang <alan.huang@liferay.com>
2024-01-04 06:56:41 +0800

    LPS-204924 Move package, *TestUtil.java should be in test.util

--- modules/apps/commerce/commerce-test/src/testIntegration/java/com/liferay/commerce/product/test/util/CommerceProductTestUtil.java
+++ modules/apps/commerce/commerce-test/src/testIntegration/java/com/liferay/commerce/product/test/util/CommerceProductTestUtil.java
@@ -3,7 +3,7 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-package com.liferay.commerce.product;
+package com.liferay.commerce.product.test.util;
 
 import com.liferay.commerce.product.option.CommerceOptionValue;
 
