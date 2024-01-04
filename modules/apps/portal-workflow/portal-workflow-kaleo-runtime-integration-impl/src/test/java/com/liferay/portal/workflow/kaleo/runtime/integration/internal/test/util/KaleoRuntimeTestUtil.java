e04c5df26f5dbf11161cf379816fc9874804a131
Qi Zhang <qi.zhang@liferay.com>
2024-01-04 10:59:19 +0800

    LPS-204924 Move package, *TestUtil.java should be in test.util

--- modules/apps/portal-workflow/portal-workflow-kaleo-runtime-integration-impl/src/test/java/com/liferay/portal/workflow/kaleo/runtime/integration/internal/test/util/KaleoRuntimeTestUtil.java
+++ modules/apps/portal-workflow/portal-workflow-kaleo-runtime-integration-impl/src/test/java/com/liferay/portal/workflow/kaleo/runtime/integration/internal/test/util/KaleoRuntimeTestUtil.java
@@ -3,7 +3,7 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-package com.liferay.portal.workflow.kaleo.runtime.integration.internal.util;
+package com.liferay.portal.workflow.kaleo.runtime.integration.internal.test.util;
 
 import com.liferay.portal.kernel.workflow.WorkflowTaskAssignee;
 import com.liferay.portal.workflow.kaleo.model.KaleoTaskAssignmentInstance;
