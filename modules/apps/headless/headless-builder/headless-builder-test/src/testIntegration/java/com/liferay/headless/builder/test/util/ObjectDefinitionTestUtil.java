9f7ffa03f8a6d525d8a31f031563edf0d162912a
Qi Zhang <qi.zhang@liferay.com>
2024-01-04 10:38:36 +0800

    LPS-204924 Move package, *TestUtil.java should be in test.util

--- modules/apps/headless/headless-builder/headless-builder-test/src/testIntegration/java/com/liferay/headless/builder/test/util/ObjectDefinitionTestUtil.java
+++ modules/apps/headless/headless-builder/headless-builder-test/src/testIntegration/java/com/liferay/headless/builder/test/util/ObjectDefinitionTestUtil.java
@@ -3,7 +3,7 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-package com.liferay.headless.builder.util;
+package com.liferay.headless.builder.test.util;
 
 import com.liferay.object.constants.ObjectDefinitionConstants;
 import com.liferay.object.model.ObjectDefinition;
