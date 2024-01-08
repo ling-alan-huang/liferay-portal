90616ebffc283012f23d6660ce5579853b2558f9
Qi Zhang <qi.zhang@liferay.com>
2024-01-08 19:04:29 +0800

    LPS-204924 Move package, *TestUtil.java should be in test.util

--- modules/apps/document-library/document-library-uad-test/src/testIntegration/java/com/liferay/document/library/uad/test/util/DLFolderUADTestUtil.java
+++ modules/apps/document-library/document-library-uad-test/src/testIntegration/java/com/liferay/document/library/uad/test/util/DLFolderUADTestUtil.java
@@ -3,7 +3,7 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-package com.liferay.document.library.uad.test;
+package com.liferay.document.library.uad.test.util;
 
 import com.liferay.document.library.kernel.model.DLFolder;
 import com.liferay.document.library.kernel.service.DLAppLocalService;
