039a04c355b44c90756720f57601beb143862df2
Seiphon Wang <871358932@qq.com>
2024-01-12 19:11:24 +0800

    LPS-205400 Add JavaTestServiceUtilCheck

--- modules/util/source-formatter/src/main/java/com/liferay/source/formatter/check/JavaTestServiceUtilCheck.java
+++ modules/util/source-formatter/src/main/java/com/liferay/source/formatter/check/JavaTestServiceUtilCheck.java
@@ -1,35 +1,150 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2023 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
 package com.liferay.source.formatter.check;
 
-import java.util.List;
-
+import com.liferay.petra.string.CharPool;
+import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.source.formatter.check.util.JavaSourceUtil;
+import com.liferay.source.formatter.parser.JavaClass;
+import com.liferay.source.formatter.parser.JavaClassParser;
+import com.liferay.source.formatter.parser.JavaMethod;
 import com.liferay.source.formatter.parser.JavaTerm;
+import com.liferay.source.formatter.util.FileUtil;
+import com.liferay.source.formatter.util.SourceFormatterUtil;
 
-public class JavaTestServiceUtilCheck extends BaseJavaTermCheck {
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * @author Seiphon Wang
+ */
+public class JavaTestServiceUtilCheck extends BaseFileCheck {
+
+	@Override
+	public boolean isLiferaySourceCheck() {
+		return true;
+	}
 
 	@Override
 	protected String doProcess(
-		String fileName, String absolutePath, JavaTerm javaTerm,
-		String fileContent) throws Exception {
+			String fileName, String absolutePath, String content)
+				throws Exception {
 
 		if (!fileName.endsWith("Test.java")) {
-			return javaTerm.getContent();
+			return content;
 		}
 
-		List<String> importNames = JavaSourceUtil.getImportNames(fileContent);
+		List<String> importNames = JavaSourceUtil.getImportNames(content);;
 
-		System.out.println("This is a test from Seiphon!");
+		Map<String, String> serviceUtilFileMap = _getServiceUtilFileMap();
+
+		Set<String> serviceUtilFileNames = serviceUtilFileMap.keySet();
 
 		for (String importName : importNames) {
-			System.out.println(importName);
+			if (!serviceUtilFileNames.contains(importName)) {
+				continue;
+			}
+
+			String className =
+				importName.substring(importName.lastIndexOf(".") + 1);
+
+			Pattern serviceUtilMethodCallPattern = Pattern.compile(
+				className + ".(\\w+)\\(");
+
+			Matcher matcher = serviceUtilMethodCallPattern.matcher(content);
+
+			while (matcher.find()) {
+				String methodName = matcher.group(1);
+
+				String methodCall =
+					JavaSourceUtil.getMethodCall(content, matcher.start());
+
+				List<String> parameters =
+					JavaSourceUtil.getParameterNames(methodCall);
+
+				System.out.println(parameters.toString());
+
+				String serviceFile = serviceUtilFileMap.get(importName);
+
+				File file = new File(serviceFile);
+
+				JavaClass serviceJavaClass = JavaClassParser.parseJavaClass(
+					serviceFile, FileUtil.read(file));
+
+				List<JavaTerm> childJavaTerms =
+					serviceJavaClass.getChildJavaTerms();
+
+				for (JavaTerm childJavaTerm : childJavaTerms) {
+					if (!childJavaTerm.isJavaMethod()) {
+						continue;
+					}
+
+					JavaMethod javaMethod = (JavaMethod)childJavaTerm;
+
+					if (methodName.equals(javaMethod.getName())) {
+						Pattern MethodPattern = Pattern.compile(
+								methodName + "\\((\\w+)");
+
+						Matcher methodMatcher =
+							MethodPattern.matcher(javaMethod.getContent());
+
+//						if (parameters.size() == parameters2.size()) {
+//							System.out.println("********" + parameters.size());
+//						}
+					}
+				}
+			}
 		}
 
-		return javaTerm.getContent();
+		return content;
 	}
 
-	@Override
-	protected String[] getCheckableJavaTermNames() {
-		return null;
+	private synchronized Map<String, String>_getServiceUtilFileMap() throws IOException {
+		if (_serviceUtilFileMap != null) {
+			return _serviceUtilFileMap;
+		}
+
+		_serviceUtilFileMap = new HashMap<String, String>();
+
+		File portalDir = getPortalDir();
+
+		List<String> serviceUtilFiles = SourceFormatterUtil.scanForFileNames(
+			portalDir.getCanonicalPath(), new String[] {"**/*ServiceUtil.java"});
+
+		for (String serviceUtilFileName : serviceUtilFiles) {
+			serviceUtilFileName = StringUtil.replace(
+				serviceUtilFileName, CharPool.BACK_SLASH, CharPool.SLASH);
+
+			String serviceFileName = StringUtil.replace(
+				serviceUtilFileName, "ServiceUtil", "Service");
+
+			File file = new File(serviceFileName);
+
+			if (!file.exists()) {
+				continue;
+			}
+
+			String packageName =
+				JavaSourceUtil.getPackageName(FileUtil.read(file));
+
+			String className = JavaSourceUtil.getClassName(serviceUtilFileName);
+
+			_serviceUtilFileMap.put(
+				packageName + "." + className, serviceFileName);
+		}
+
+		return _serviceUtilFileMap;
 	}
+
+	private Map<String, String> _serviceUtilFileMap;
 
 }
