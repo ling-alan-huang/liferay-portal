6aef50bb232bbb863b5447ad8207c1da90d3ea2a
Seiphon Wang <871358932@qq.com>
2024-01-17 20:38:14 +0800

    LPS-206304 SF

--- modules/util/source-formatter/src/main/java/com/liferay/source/formatter/check/JavaRemoteServiceCheck.java
+++ modules/util/source-formatter/src/main/java/com/liferay/source/formatter/check/JavaRemoteServiceCheck.java
@@ -1,18 +1,16 @@
 /**
- * SPDX-FileCopyrightText: (c) 2000 Liferay, Inc. https://liferay.com
+ * SPDX-FileCopyrightText: (c) 2024 Liferay, Inc. https://liferay.com
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
 package com.liferay.source.formatter.check;
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
-import com.liferay.petra.string.StringBundler;
-import com.liferay.petra.string.StringUtil;
 import com.liferay.source.formatter.check.util.JavaSourceUtil;
 import com.liferay.source.formatter.parser.JavaMethod;
 import com.liferay.source.formatter.parser.JavaTerm;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * @author Seiphon Wang
@@ -26,8 +24,9 @@ public class JavaRemoteServiceCheck extends BaseJavaTermCheck {
 
 	@Override
 	protected String doProcess(
-		String fileName, String absolutePath, JavaTerm javaTerm,
-		String fileContent) throws Exception {
+			String fileName, String absolutePath, JavaTerm javaTerm,
+			String fileContent)
+		throws Exception {
 
 		if (!javaTerm.isJavaMethod()) {
 			return javaTerm.getContent();
@@ -36,40 +35,12 @@ public class JavaRemoteServiceCheck extends BaseJavaTermCheck {
 		String className = JavaSourceUtil.getClassName(fileName);
 
 		if (!className.endsWith("ServiceImpl") ||
-				className.endsWith("LocalServiceImpl")) {
+			className.endsWith("LocalServiceImpl")) {
+
 			return javaTerm.getContent();
 		}
 
-		JavaMethod javaMethod = (JavaMethod)javaTerm;
-
-		javaMethod.getContent();
-
-		Pattern methodPattern = Pattern.compile(
-			javaMethod.getName() + "\\s*\\(([^)]*)\\)",
-			Pattern.MULTILINE);
-
-		Matcher matcher = methodPattern.matcher(
-			javaMethod.getContent());
-
-		if (matcher.find()) {
-			String parameterString = matcher.group(1);
-
-			String[] parameters =
-				parameterString.split(",");
-
-			for (String parameter : parameters) {
-				parameter = parameter.trim();
-
-				if (parameter.equals("long userId")) {
-					addMessage(
-						fileName,
-						StringBundler.concat(
-							"Please don't expose 'long userId' in remote ", 
-							"service, use 'getUserId()' instead."),
-						javaTerm.getLineNumber());
-				}
-			}
-		}
+		_checkExposedUserId(fileName, javaTerm);
 
 		return javaTerm.getContent();
 	}
@@ -77,6 +48,35 @@ public class JavaRemoteServiceCheck extends BaseJavaTermCheck {
 	@Override
 	protected String[] getCheckableJavaTermNames() {
 		return new String[] {JAVA_METHOD};
+	}
+
+	private void _checkExposedUserId(String fileName, JavaTerm javaTerm) {
+		JavaMethod javaMethod = (JavaMethod)javaTerm;
+
+		javaMethod.getContent();
+
+		Pattern methodPattern = Pattern.compile(
+			javaMethod.getName() + "\\s*\\(([^)]*)\\)", Pattern.MULTILINE);
+
+		Matcher matcher = methodPattern.matcher(javaMethod.getContent());
+
+		if (matcher.find()) {
+			String parameterString = matcher.group(1);
+
+			String[] parameters = parameterString.split(",");
+
+			for (String parameter : parameters) {
+				parameter = parameter.trim();
+
+				if (parameter.equals("long userId")) {
+					addMessage(
+						fileName,
+						"Please do not expose 'long userId' in remote " +
+							"service, use 'getUserId()' instead.",
+						javaTerm.getLineNumber());
+				}
+			}
+		}
 	}
 
 }
