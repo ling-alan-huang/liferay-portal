3291aa418ef51c7ed478d874418feca287393fbc
Alan Huang <alan.huang@liferay.com>
2024-01-26 14:26:27 +0800

    LPS-205913 SF

--- modules/apps/headless/headless-delivery/headless-delivery-test/src/testIntegration/java/com/liferay/headless/delivery/resource/v1_0/test/NavigationMenuResourceTest.java
+++ modules/apps/headless/headless-delivery/headless-delivery-test/src/testIntegration/java/com/liferay/headless/delivery/resource/v1_0/test/NavigationMenuResourceTest.java
@@ -265,11 +265,11 @@ public class NavigationMenuResourceTest
 		NavigationMenuItem navigationMenuItem =
 			getNavigationMenu.getNavigationMenuItems()[0];
 
+		String url = navigationMenuItem.getContentURL();
+
 		Assert.assertTrue(
-			navigationMenuItem.getContentURL(
-			).contains(
-				"/headless-delivery/v1.0/" + contentURL
-			));
+			url.contains("/headless-delivery/v1.0/" + contentURL));
+
 		Assert.assertEquals(
 			siteNavigationMenuItem.getSiteNavigationMenuItemId(),
 			GetterUtil.getLong(navigationMenuItem.getId()));
@@ -338,11 +338,11 @@ public class NavigationMenuResourceTest
 		assertEquals(postNavigationMenu, page.fetchFirstItem());
 		assertValid(page);
 
+		String url = navigationMenuItem.getContentURL();
+
 		Assert.assertTrue(
-			navigationMenuItem.getContentURL(
-			).contains(
-				"/headless-delivery/v1.0/" + contentURL
-			));
+			url.contains("/headless-delivery/v1.0/" + contentURL));
+
 		Assert.assertEquals(
 			siteNavigationMenuItem.getSiteNavigationMenuItemId(),
 			GetterUtil.getLong(navigationMenuItem.getId()));
