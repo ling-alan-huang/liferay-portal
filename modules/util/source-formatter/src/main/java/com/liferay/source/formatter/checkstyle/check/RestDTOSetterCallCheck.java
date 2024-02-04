93876642714f9b7d34624f13e3d972c0e6d06aee
Alan Huang <alan.huang@liferay.com>
2024-02-04 23:54:35 +0800

    Simplify

--- modules/util/source-formatter/src/main/java/com/liferay/source/formatter/checkstyle/check/RestDTOSetterCallCheck.java
+++ modules/util/source-formatter/src/main/java/com/liferay/source/formatter/checkstyle/check/RestDTOSetterCallCheck.java
@@ -1,33 +1,19 @@
 /**
- * SPDX-FileCopyrightText: (c) 2000 Liferay, Inc. https://liferay.com
+ * SPDX-FileCopyrightText: (c) 2024 Liferay, Inc. https://liferay.com
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
 package com.liferay.source.formatter.checkstyle.check;
 
-import com.liferay.portal.kernel.log.Log;
-import com.liferay.portal.kernel.log.LogFactoryUtil;
-import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.source.formatter.check.util.BNDSourceUtil;
 import com.liferay.source.formatter.check.util.JavaSourceUtil;
 import com.liferay.source.formatter.check.util.SourceUtil;
-import com.liferay.source.formatter.checkstyle.check.BaseAPICheck.MethodCall;
-import com.liferay.source.formatter.parser.JavaClass;
-import com.liferay.source.formatter.parser.JavaClassParser;
-import com.liferay.source.formatter.parser.JavaMethod;
-import com.liferay.source.formatter.parser.JavaParameter;
-import com.liferay.source.formatter.parser.JavaSignature;
-import com.liferay.source.formatter.parser.JavaTerm;
-import com.liferay.source.formatter.parser.ParseException;
-import com.liferay.source.formatter.util.FileUtil;
 
 import com.puppycrawl.tools.checkstyle.api.DetailAST;
 import com.puppycrawl.tools.checkstyle.api.TokenTypes;
 
 import java.io.File;
-import java.io.IOException;
 
-import java.util.List;
 import java.util.Map;
 
 /**
@@ -53,11 +39,11 @@ public class RestDTOSetterCallCheck extends BaseCheck {
 		for (DetailAST childDetailAST :
 				getAllChildTokens(detailAST, true, TokenTypes.METHOD_CALL)) {
 
-			_checkExprStatement(absolutePath, childDetailAST);
+			_checkSetterCall(absolutePath, childDetailAST);
 		}
 	}
 
-	private void _checkExprStatement(String absolutePath, DetailAST detailAST) {
+	private void _checkSetterCall(String absolutePath, DetailAST detailAST) {
 		DetailAST dotDetailAST = detailAST.findFirstToken(TokenTypes.DOT);
 
 		if (dotDetailAST == null) {
@@ -75,11 +61,10 @@ public class RestDTOSetterCallCheck extends BaseCheck {
 		String variableTypeName = getVariableTypeName(
 			detailAST, variableName, false, false, true);
 
-		if (!variableTypeName.startsWith("com.liferay.") || !variableTypeName.contains(".dto.v")) {
-			return;
-		}
+		if (!variableTypeName.startsWith("com.liferay.") ||
+			!variableTypeName.contains(".dto.v") ||
+			!_isRestDTO(absolutePath, variableTypeName)) {
 
-		if (!_isRestDTO(absolutePath, variableTypeName)) {
 			return;
 		}
 
@@ -99,29 +84,28 @@ public class RestDTOSetterCallCheck extends BaseCheck {
 
 		if (parentDetailAST.getType() == TokenTypes.LITERAL_IF) {
 			log(
-					detailAST, _MSG_INLINE_IF_STATEMENT, methodName,
-					"UnsafeSupplier");
+				detailAST, _MSG_INLINE_IF_STATEMENT, methodName,
+				"UnsafeSupplier");
 		}
 		else {
 			DetailAST elistDetailAST = detailAST.findFirstToken(
-					TokenTypes.ELIST);
+				TokenTypes.ELIST);
 
-				DetailAST childDetailAST = elistDetailAST.getFirstChild();
+			DetailAST childDetailAST = elistDetailAST.getFirstChild();
 
-				if ((childDetailAST == null) ||
-					(childDetailAST.getType() == TokenTypes.LAMBDA) ||
-					(childDetailAST.findFirstToken(TokenTypes.METHOD_REF) !=
-						null)) {
+			if ((childDetailAST == null) ||
+				(childDetailAST.getType() == TokenTypes.LAMBDA) ||
+				(childDetailAST.findFirstToken(TokenTypes.METHOD_REF) !=
+					null)) {
 
-					return;
-				}
+				return;
+			}
 
 			log(
-					detailAST, _MSG_USE_SET_METHOD_INSTEAD, methodName,
-					"UnsafeSupplier");
+				detailAST, _MSG_USE_SET_METHOD_INSTEAD, methodName,
+				"UnsafeSupplier");
 		}
 	}
-
 
 	private synchronized Map<String, String> _getBundleSymbolicNamesMap(
 		String absolutePath) {
@@ -134,46 +118,6 @@ public class RestDTOSetterCallCheck extends BaseCheck {
 		return _bundleSymbolicNamesMap;
 	}
 
-
-	private boolean _isRestDTO(
-			String absolutePath, String fullyQualifiedTypeName)
-		{
-
-		if (fullyQualifiedTypeName == null) {
-			return false;
-		}
-
-		File javaFile = JavaSourceUtil.getJavaFile(
-			fullyQualifiedTypeName, _getRootDirName(absolutePath),
-			_getBundleSymbolicNamesMap(absolutePath));
-
-		if (javaFile == null) {
-			return false;
-		}
-
-		String javaFileAbsolutePath = SourceUtil.getAbsolutePath(javaFile);
-		
-		int x = javaFileAbsolutePath.lastIndexOf("-api/src/main/");
-
-		if (x == -1) {
-			x = javaFileAbsolutePath.lastIndexOf("-client/src/main/");
-		}
-
-		if (x == -1) {
-			return false;
-		}
-
-		String restOpenAPIFilePath = javaFileAbsolutePath.substring(0, x) + "-impl/rest-openapi.yaml";
-		
-		File file = new File(restOpenAPIFilePath);
-
-		if (!file.exists()) {
-			return false;
-		}
-
-		return true;
-	}
-
 	private synchronized String _getRootDirName(String absolutePath) {
 		if (_rootDirName != null) {
 			return _rootDirName;
@@ -184,14 +128,48 @@ public class RestDTOSetterCallCheck extends BaseCheck {
 		return _rootDirName;
 	}
 
+	private boolean _isRestDTO(String absolutePath, String variableTypeName) {
+		if (variableTypeName == null) {
+			return false;
+		}
+
+		File javaFile = JavaSourceUtil.getJavaFile(
+			variableTypeName, _getRootDirName(absolutePath),
+			_getBundleSymbolicNamesMap(absolutePath));
+
+		if (javaFile == null) {
+			return false;
+		}
+
+		String javaFileAbsolutePath = SourceUtil.getAbsolutePath(javaFile);
+
+		int x = javaFileAbsolutePath.lastIndexOf("-api/src/main/");
+
+		if (x == -1) {
+			x = javaFileAbsolutePath.lastIndexOf("-client/src/main/");
+		}
+
+		if (x == -1) {
+			return false;
+		}
+
+		String restOpenAPIFilePath =
+			javaFileAbsolutePath.substring(0, x) + "-impl/rest-openapi.yaml";
+
+		File file = new File(restOpenAPIFilePath);
+
+		if (!file.exists()) {
+			return false;
+		}
+
+		return true;
+	}
+
 	private static final String _MSG_INLINE_IF_STATEMENT =
 		"if.statement.inline";
 
 	private static final String _MSG_USE_SET_METHOD_INSTEAD =
 		"set.method.use.instead";
-
-	private static final Log _log = LogFactoryUtil.getLog(
-		RestDTOSetterCallCheck.class);
 
 	private volatile Map<String, String> _bundleSymbolicNamesMap;
 	private volatile String _rootDirName;
