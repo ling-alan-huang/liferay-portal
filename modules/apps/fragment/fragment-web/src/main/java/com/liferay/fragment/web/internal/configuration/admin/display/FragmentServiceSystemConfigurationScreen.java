cc33396669c574c75f03d2a3937073dda012f086
Alan Huang <alan.huang@liferay.com>
2024-03-17 17:17:57 +0800

    LPD-20678 Move package, add .admin

--- modules/apps/fragment/fragment-web/src/main/java/com/liferay/fragment/web/internal/configuration/admin/display/FragmentServiceSystemConfigurationScreen.java
+++ modules/apps/fragment/fragment-web/src/main/java/com/liferay/fragment/web/internal/configuration/admin/display/FragmentServiceSystemConfigurationScreen.java
@@ -3,7 +3,7 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-package com.liferay.fragment.web.internal.configuration.display;
+package com.liferay.fragment.web.internal.configuration.admin.display;
 
 import com.liferay.configuration.admin.display.ConfigurationScreen;
 import com.liferay.portal.configuration.metatype.annotations.ExtendedObjectClassDefinition;
@@ -14,12 +14,12 @@ import org.osgi.service.component.annotations.Component;
  * @author Eudaldo Alonso
  */
 @Component(service = ConfigurationScreen.class)
-public class FragmentServiceCompanyConfigurationScreen
+public class FragmentServiceSystemConfigurationScreen
 	extends BaseFragmentServiceConfigurationScreen {
 
 	@Override
 	public String getScope() {
-		return ExtendedObjectClassDefinition.Scope.COMPANY.getValue();
+		return ExtendedObjectClassDefinition.Scope.SYSTEM.getValue();
 	}
 
 }
