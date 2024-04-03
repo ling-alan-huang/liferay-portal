09280f0f8c4ab094226ab87a47c6a6667bfc1886
Alan Huang <alan.huang@liferay.com>
2024-04-03 14:12:03 +0800

    LPD-21824 Do the same things in JavaUpgradeProcessFactoryCheck

--- modules/util/source-formatter/src/main/java/com/liferay/source/formatter/check/JavaUpgradeProcessFactoryCheck.java
+++ modules/util/source-formatter/src/main/java/com/liferay/source/formatter/check/JavaUpgradeProcessFactoryCheck.java
@@ -5,38 +5,27 @@
 
 package com.liferay.source.formatter.check;
 
-import com.liferay.portal.kernel.log.Log;
-import com.liferay.portal.kernel.log.LogFactoryUtil;
-import com.liferay.portal.kernel.util.ArrayUtil;
 import com.liferay.portal.kernel.util.StringUtil;
+import com.liferay.portal.kernel.util.Validator;
+import com.liferay.source.formatter.BNDSettings;
+import com.liferay.source.formatter.check.util.BNDSourceUtil;
 import com.liferay.source.formatter.check.util.JavaSourceUtil;
 import com.liferay.source.formatter.check.util.SourceUtil;
-import com.liferay.source.formatter.parser.JavaTerm;
 import com.liferay.source.formatter.util.FileUtil;
 
 import java.io.File;
 import java.io.IOException;
 
-import java.nio.file.FileVisitOption;
-import java.nio.file.FileVisitResult;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.SimpleFileVisitor;
-import java.nio.file.attribute.BasicFileAttributes;
-
-import java.util.ArrayList;
-import java.util.EnumSet;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import org.dom4j.Document;
 import org.dom4j.Element;
 
 /**
  * @author Alan Huang
  */
-public class JavaUpgradeProcessFactoryCheck extends BaseJavaTermCheck {
+public class JavaUpgradeProcessFactoryCheck extends BaseFileCheck {
 
 	@Override
 	public boolean isLiferaySourceCheck() {
@@ -45,56 +34,10 @@ public class JavaUpgradeProcessFactoryCheck extends BaseJavaTermCheck {
 
 	@Override
 	protected String doProcess(
-		String fileName, String absolutePath, JavaTerm javaTerm,
-		String fileContent) {
+			String fileName, String absolutePath, String content)
+		throws IOException {
 
-		return _sortMethodCalls(javaTerm.getContent());
-	}
-
-	@Override
-	protected String[] getCheckableJavaTermNames() {
-		return new String[] {JAVA_METHOD};
-	}
-
-	private int _getColumnIndex(Element entityElement, String columnName) {
-		int i = 0;
-
-		for (Element columnElement :
-				(List<Element>)entityElement.elements("column")) {
-
-			if (StringUtil.equalsIgnoreCase(
-					columnName, columnElement.attributeValue("name"))) {
-
-				return i;
-			}
-
-			i++;
-		}
-
-		return -1;
-	}
-
-	private synchronized List<Element> _getServiceXMLElements() {
-		if (_serviceXMLElements != null) {
-			return _serviceXMLElements;
-		}
-
-		_serviceXMLElements = new ArrayList<>();
-
-		try {
-			_populateServiceXMLElements("modules/apps", 6);
-			_populateServiceXMLElements("modules/dxp/apps", 6);
-			_populateServiceXMLElements("portal-impl/src/com/liferay", 4);
-		}
-		catch (IOException ioException) {
-			if (_log.isDebugEnabled()) {
-				_log.debug(ioException);
-			}
-
-			return null;
-		}
-
-		return _serviceXMLElements;
+		return _sortMethodCalls(fileName, absolutePath, content);
 	}
 
 	private int _getTableIndex(Element serviceXMLElement, String tableName) {
@@ -113,59 +56,28 @@ public class JavaUpgradeProcessFactoryCheck extends BaseJavaTermCheck {
 		return -1;
 	}
 
-	private void _populateServiceXMLElements(String dirName, int maxDepth)
+	private String _sortMethodCalls(
+			String fileName, String absolutePath, String content)
 		throws IOException {
 
-		File directory = getFile(dirName, getMaxDirLevel());
+		String packagePath = "";
 
-		if (directory == null) {
-			return;
+		if (absolutePath.contains("/portal-impl/")) {
+			packagePath = "portal-impl";
 		}
+		else {
+			BNDSettings bndSettings = getBNDSettings(fileName);
 
-		final List<File> serviceXMLFiles = new ArrayList<>();
-
-		Files.walkFileTree(
-			directory.toPath(), EnumSet.noneOf(FileVisitOption.class), maxDepth,
-			new SimpleFileVisitor<Path>() {
-
-				@Override
-				public FileVisitResult preVisitDirectory(
-					Path dirPath, BasicFileAttributes basicFileAttributes) {
-
-					String dirName = String.valueOf(dirPath.getFileName());
-
-					if (ArrayUtil.contains(_SKIP_DIR_NAMES, dirName)) {
-						return FileVisitResult.SKIP_SUBTREE;
-					}
-
-					Path path = dirPath.resolve("service.xml");
-
-					if (Files.exists(path)) {
-						serviceXMLFiles.add(path.toFile());
-
-						return FileVisitResult.SKIP_SUBTREE;
-					}
-
-					return FileVisitResult.CONTINUE;
-				}
-
-			});
-
-		for (File serviceXMLFile : serviceXMLFiles) {
-			Document serviceXMLDocument = SourceUtil.readXML(
-				FileUtil.read(serviceXMLFile));
-
-			if (serviceXMLDocument == null) {
-				continue;
+			if (bndSettings == null) {
+				return content;
 			}
 
-			Element serviceXMLElement = serviceXMLDocument.getRootElement();
+			String bundleSymbolicName = BNDSourceUtil.getDefinitionValue(
+				bndSettings.getContent(), "Bundle-SymbolicName");
 
-			_serviceXMLElements.add(serviceXMLElement);
+			packagePath = StringUtil.removeLast(bundleSymbolicName, ".service");
 		}
-	}
 
-	private String _sortMethodCalls(String content) {
 		Matcher matcher1 = _methodCallsPattern.matcher(content);
 
 		while (matcher1.find()) {
@@ -204,63 +116,70 @@ public class JavaUpgradeProcessFactoryCheck extends BaseJavaTermCheck {
 					continue;
 				}
 
-				List<Element> serviceXMLElements = _getServiceXMLElements();
+				populateModelInformations();
+
+				Object[] modelInformation = getModelInformation(packagePath);
+
+				if (modelInformation == null) {
+					continue;
+				}
+
+				String tablesSQLFileLocation = (String)modelInformation[1];
+
+				if (Validator.isNull(tablesSQLFileLocation)) {
+					continue;
+				}
+
+				File file = new File(tablesSQLFileLocation);
+
+				if (!file.exists()) {
+					continue;
+				}
+
+				String tablesSQLContent = FileUtil.read(file);
+
+				if (tablesSQLContent == null) {
+					continue;
+				}
 
 				if (tableName.equals(previousTableName)) {
-					for (Element serviceXMLElement : serviceXMLElements) {
-						int index1 = -1;
-						int index2 = -1;
+					int index1 = SourceUtil.getColumnIndex(
+						tablesSQLContent, tableName, previousColumnName);
+					int index2 = SourceUtil.getColumnIndex(
+						tablesSQLContent, tableName, columnName);
 
-						for (Element entityElement :
-								(List<Element>)serviceXMLElement.elements(
-									"entity")) {
+					if ((index2 != -1) && (index1 > index2)) {
+						int x = matcher2.start();
 
-							if (!tableName.equals(
-									entityElement.attributeValue("name"))) {
+						int y = content.lastIndexOf(previousMatch, x);
 
-								continue;
-							}
+						content = StringUtil.replaceFirst(
+							content, match, previousMatch,
+							matcher1.start() + x);
 
-							index1 = _getColumnIndex(
-								entityElement, previousColumnName);
-							index2 = _getColumnIndex(entityElement, columnName);
-						}
-
-						if ((index2 != -1) && (index1 > index2)) {
-							int x = matcher2.start();
-
-							int y = content.lastIndexOf(previousMatch, x);
-
-							content = StringUtil.replaceFirst(
-								content, match, previousMatch,
-								matcher1.start() + x);
-
-							return StringUtil.replaceFirst(
-								content, previousMatch, match,
-								matcher1.start() + y);
-						}
+						return StringUtil.replaceFirst(
+							content, previousMatch, match,
+							matcher1.start() + y);
 					}
 				}
 				else {
-					for (Element serviceXMLElement : serviceXMLElements) {
-						int index1 = _getTableIndex(
-							serviceXMLElement, previousTableName);
-						int index2 = _getTableIndex(
-							serviceXMLElement, tableName);
+					int index1 = _getTableIndex(
+						(Element)modelInformation[0], previousTableName);
+					int index2 = _getTableIndex(
+						(Element)modelInformation[0], tableName);
 
-						if ((index2 != -1) && (index1 > index2)) {
-							int x = matcher2.start();
+					if ((index2 != -1) && (index1 > index2)) {
+						int x = matcher2.start();
 
-							int y = content.lastIndexOf(previousMatch, x);
+						int y = content.lastIndexOf(previousMatch, x);
 
-							content = StringUtil.replaceFirst(
-								content, match, previousMatch,
-								matcher1.start() + x);
+						content = StringUtil.replaceFirst(
+							content, match, previousMatch,
+							matcher1.start() + x);
 
-							return StringUtil.replaceFirst(
-								content, previousMatch, match,
-								matcher1.start() + y);
-						}
+						return StringUtil.replaceFirst(
+							content, previousMatch, match,
+							matcher1.start() + y);
 					}
 				}
 
@@ -274,20 +193,9 @@ public class JavaUpgradeProcessFactoryCheck extends BaseJavaTermCheck {
 		return content;
 	}
 
-	private static final String[] _SKIP_DIR_NAMES = {
-		".git", ".gradle", ".idea", ".m2", ".settings", "bin", "build",
-		"classes", "dependencies", "node_modules", "node_modules_cache", "sql",
-		"src", "test", "test-classes", "test-coverage", "test-results", "tmp"
-	};
-
-	private static final Log _log = LogFactoryUtil.getLog(
-		JavaUpgradeProcessFactoryCheck.class);
-
 	private static final Pattern _methodCallPattern = Pattern.compile(
 		"UpgradeProcessFactory\\.(\\w+)\\([^;]+?\\)(?=,|\\))");
 	private static final Pattern _methodCallsPattern = Pattern.compile(
 		"([ \\t]*UpgradeProcessFactory\\.\\w+\\([^;]+?\\)(,|\\))\\s*){2,}");
-
-	private List<Element> _serviceXMLElements;
 
 }
