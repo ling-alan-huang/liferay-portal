24fac3528e3b6b06eed3aab89fef5f424b873ca4
Alan Huang <alan.huang@liferay.com>
2024-04-23 20:51:02 +0800

    LPD-23646 Only apply check to modules

--- modules/util/source-formatter/src/main/java/com/liferay/source/formatter/check/JavaModuleMissingUpgradeProcessTestCheck.java
+++ modules/util/source-formatter/src/main/java/com/liferay/source/formatter/check/JavaModuleMissingUpgradeProcessTestCheck.java
@@ -1,5 +1,5 @@
 /**
- * SPDX-FileCopyrightText: (c) 2000 Liferay, Inc. https://liferay.com
+ * SPDX-FileCopyrightText: (c) 2024 Liferay, Inc. https://liferay.com
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
@@ -21,7 +21,7 @@ import java.util.regex.Pattern;
 /**
  * @author Alan Huang
  */
-public class JavaMissingUpgradeProcessTestCheck extends BaseFileCheck {
+public class JavaModuleMissingUpgradeProcessTestCheck extends BaseFileCheck {
 
 	@Override
 	public boolean isLiferaySourceCheck() {
