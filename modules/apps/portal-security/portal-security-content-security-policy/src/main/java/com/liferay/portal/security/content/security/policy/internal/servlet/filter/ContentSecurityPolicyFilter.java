b7896af4bd22ea029fc9b1741c68d6c216042c2b
Iván Zaera Avellón <ivan.zaera@liferay.com>
2024-07-24 14:08:47 +0200

    LPD-33708 Remove <style> rewriting in CSP filter

--- modules/apps/portal-security/portal-security-content-security-policy/src/main/java/com/liferay/portal/security/content/security/policy/internal/servlet/filter/ContentSecurityPolicyFilter.java
+++ modules/apps/portal-security/portal-security-content-security-policy/src/main/java/com/liferay/portal/security/content/security/policy/internal/servlet/filter/ContentSecurityPolicyFilter.java
@@ -138,10 +138,6 @@ public class ContentSecurityPolicyFilter extends BasePortalFilter {
 			"<(?i)link ", "<link " + nonceAttribute + " ");
 		content = content.replaceAll(
 			"<(?i)link>", "<link " + nonceAttribute + "");
-		content = content.replaceAll(
-			"<(?i)style ", "<style " + nonceAttribute + " ");
-		content = content.replaceAll(
-			"<(?i)style>", "<style " + nonceAttribute + ">");
 
 		Pattern pattern = Pattern.compile(
 			"\\{.*nonce=\".{" + nonce.length() + "}\".*\\}");
