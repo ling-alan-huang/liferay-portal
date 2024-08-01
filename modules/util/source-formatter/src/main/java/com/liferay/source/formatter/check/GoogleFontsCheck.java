1c4eb78f924ef260162cb8465a712c752f76eabf
Alan Huang <alan.huang@liferay.com>
2024-08-01 12:45:03 +0800

    LPD-32865 Add GoogleFontsCheck

---
+++ modules/util/source-formatter/src/main/java/com/liferay/source/formatter/check/GoogleFontsCheck.java
@@ -0,0 +1,26 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2024 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.source.formatter.check;
+
+/**
+ * @author Alan Huang
+ */
+public class GoogleFontsCheck extends BaseFileCheck {
+
+	@Override
+	protected String doProcess(
+		String fileName, String absolutePath, String content) {
+
+		int x = content.indexOf("fonts.googleapis.com");
+
+		if (x != -1) {
+			addMessage(fileName, "Do not use Google fonts");
+		}
+
+		return content;
+	}
+
+}
