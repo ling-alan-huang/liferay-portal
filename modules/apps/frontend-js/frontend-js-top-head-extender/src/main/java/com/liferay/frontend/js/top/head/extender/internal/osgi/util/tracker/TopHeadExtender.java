01447bb2e2934aef62fbfe8856e6ba3b2fc20410
Alan Huang <alan.huang@liferay.com>
2024-09-11 23:14:03 +0800

    LPD-36028 Move package to osgi.util.tracker

--- modules/apps/frontend-js/frontend-js-top-head-extender/src/main/java/com/liferay/frontend/js/top/head/extender/internal/osgi/util/tracker/TopHeadExtender.java
+++ modules/apps/frontend-js/frontend-js-top-head-extender/src/main/java/com/liferay/frontend/js/top/head/extender/internal/osgi/util/tracker/TopHeadExtender.java
@@ -3,7 +3,7 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-package com.liferay.frontend.js.top.head.extender.internal;
+package com.liferay.frontend.js.top.head.extender.internal.osgi.util.tracker;
 
 import com.liferay.frontend.js.top.head.extender.TopHeadResources;
 import com.liferay.petra.string.StringBundler;
--- modules/apps/frontend-js/frontend-js-web/src/main/java/com/liferay/frontend/js/web/internal/osgi/util/tracker/ResourceBundleServiceTrackerCustomizer.java
+++ modules/apps/frontend-js/frontend-js-web/src/main/java/com/liferay/frontend/js/web/internal/osgi/util/tracker/ResourceBundleServiceTrackerCustomizer.java
@@ -3,7 +3,7 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-package com.liferay.frontend.js.web.internal.servlet;
+package com.liferay.frontend.js.web.internal.osgi.util.tracker;
 
 import com.liferay.frontend.js.web.internal.LanguageState;
 import com.liferay.portal.kernel.log.Log;
