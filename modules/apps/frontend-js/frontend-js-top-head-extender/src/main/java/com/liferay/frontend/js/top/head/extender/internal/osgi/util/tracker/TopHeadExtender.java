3b6b6e930b574ff74e89c4e8abfda9911b8659a4
Alan Huang <alan.huang@liferay.com>
2024-09-13 02:16:41 +0800

    LPD-36028 Move package to osgi.util.tracker

--- modules/apps/frontend-js/frontend-js-top-head-extender/src/main/java/com/liferay/frontend/js/top/head/extender/internal/osgi/util/tracker/TopHeadExtender.java
+++ modules/apps/frontend-js/frontend-js-top-head-extender/src/main/java/com/liferay/frontend/js/top/head/extender/internal/osgi/util/tracker/TopHeadExtender.java
@@ -3,9 +3,10 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-package com.liferay.frontend.js.top.head.extender.internal;
+package com.liferay.frontend.js.top.head.extender.internal.osgi.util.tracker;
 
 import com.liferay.frontend.js.top.head.extender.TopHeadResources;
+import com.liferay.frontend.js.top.head.extender.internal.TopHeadResourcesImpl;
 import com.liferay.petra.string.StringBundler;
 import com.liferay.petra.string.StringPool;
 import com.liferay.portal.kernel.util.GetterUtil;
