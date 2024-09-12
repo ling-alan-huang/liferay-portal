dd02d3492f5624a09b85724e022c5c0b6b09132e
Alan Huang <alan.huang@liferay.com>
2024-09-13 02:17:06 +0800

    LPD-36028 Move package to osgi.util.tracker

--- modules/apps/portal-search/portal-search/src/main/java/com/liferay/portal/search/internal/osgi/util/tracker/IndexOnStartupExecutor.java
+++ modules/apps/portal-search/portal-search/src/main/java/com/liferay/portal/search/internal/osgi/util/tracker/IndexOnStartupExecutor.java
@@ -3,7 +3,7 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-package com.liferay.portal.search.internal;
+package com.liferay.portal.search.internal.osgi.util.tracker;
 
 import com.liferay.portal.configuration.metatype.bnd.util.ConfigurableUtil;
 import com.liferay.portal.instance.lifecycle.PortalInstanceLifecycleListener;
