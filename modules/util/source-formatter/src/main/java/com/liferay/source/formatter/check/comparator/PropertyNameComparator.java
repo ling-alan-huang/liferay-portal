ee8e258ebbb46c124c2a782fdc2eabc4926ea178
Alan Huang <alan.huang@liferay.com>
2024-09-26 17:29:12 +0800

    LPD-33563 Auto SF

--- modules/util/source-formatter/src/main/java/com/liferay/source/formatter/check/comparator/PropertyNameComparator.java
+++ modules/util/source-formatter/src/main/java/com/liferay/source/formatter/check/comparator/PropertyNameComparator.java
@@ -29,11 +29,12 @@ public class PropertyNameComparator extends NaturalOrderStringComparator {
 		char c1 = s1.charAt(x);
 		char c2 = s2.charAt(x);
 
-		if (Character.isLetterOrDigit(c1) && c2 == CharPool.STAR ||
-				Character.isLetterOrDigit(c2) && c1 == CharPool.STAR) {
-			return -value;
+		if ((Character.isLetterOrDigit(c1) && (c2 == CharPool.STAR)) ||
+			(Character.isLetterOrDigit(c2) && (c1 == CharPool.STAR))) {
 
+			return -value;
 		}
+
 		for (char[] array : _REVERSE_ORDER_CHARACTERS) {
 			if (ArrayUtil.contains(array, c1) &&
 				ArrayUtil.contains(array, c2)) {
