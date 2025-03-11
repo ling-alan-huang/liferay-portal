12b0d2e1c5ff455789291440d838123f0da3258b
Alan Huang <alan.huang@liferay.com>
2025-03-11 20:15:15 +0800

    LPD-50859 Move package

--- modules/apps/depot/depot-web/src/main/java/com/liferay/depot/web/internal/item/selector/DepotGroupItemSelectorCriterion.java
+++ modules/apps/depot/depot-web/src/main/java/com/liferay/depot/web/internal/item/selector/DepotGroupItemSelectorCriterion.java
@@ -3,7 +3,7 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-package com.liferay.depot.web.internal.item.selector.criteria.depot.group.criterion;
+package com.liferay.depot.web.internal.item.selector;
 
 import com.liferay.item.selector.criteria.group.criterion.GroupItemSelectorCriterion;
 
