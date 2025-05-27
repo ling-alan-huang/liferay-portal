1ee43211354c6459c77386331340fa0ac742027f
Alan Huang <alan.huang@liferay.com>
2025-05-27 14:35:30 +0800

    LPD-54441 SF

--- modules/util/source-formatter/src/main/java/com/liferay/source/formatter/check/GradleJakartaCheck.java
+++ modules/util/source-formatter/src/main/java/com/liferay/source/formatter/check/GradleJakartaCheck.java
@@ -5,6 +5,10 @@
 
 package com.liferay.source.formatter.check;
 
+import com.liferay.petra.string.CharPool;
+import com.liferay.portal.kernel.util.StringUtil;
+import com.liferay.portal.tools.ToolsUtil;
+
 /**
  * @author Alan Huang
  */
@@ -13,6 +17,58 @@ public class GradleJakartaCheck extends BaseJakartaTransformerCheck {
 	@Override
 	protected String doProcess(
 		String fileName, String absolutePath, String content) {
+
+		content = _formatDeployDependencies(content);
+
+		content = replace(replacementDashDotMap, content);
+
+		return content;
+	}
+
+	private String _formatDeployDependencies(String content) {
+		int x = content.indexOf("deployDependencies {");
+
+		if (x == -1) {
+			return content;
+		}
+
+		String deployDependencies = null;
+
+		int y = x + 19;
+
+		while (true) {
+			y = content.indexOf(CharPool.CLOSE_CURLY_BRACE, y + 1);
+
+			if (y == -1) {
+				return content;
+			}
+
+			deployDependencies = content.substring(x, y + 1);
+
+			if (ToolsUtil.getLevel(deployDependencies, "{", "}") == 0) {
+				content = StringUtil.replaceFirst(
+					content, "cxf-*", "org.apache.cxf.*", x);
+				content = StringUtil.replaceFirst(
+					content, "jakarta.ws.rs-api-*", "jakarta.ws.rs-*", x);
+				content = StringUtil.replaceFirst(
+					content, "jakarta.mvc-api", "jakarta.mvc-api", x);
+				content = StringUtil.replaceFirst(
+					content, "javax\\.mvc-api", "jakarta\\.mvc-api", x);
+				content = StringUtil.replaceFirst(
+					content, "jaxb-osgi-*", "com.sun.xml.bind.jaxb.osgi-*", x);
+				content = StringUtil.replaceFirst(
+					content, "jackson-jaxrs-base-*",
+					"jackson-jakarta-rs-base-*", x);
+				content = StringUtil.replaceFirst(
+					content, "jackson-jaxrs-json-provider-*",
+					"jackson-jakarta-rs-json-provider-*", x);
+				content = StringUtil.replaceFirst(
+					content, "jackson-module-jaxb-annotations-*",
+					"jackson-module-jakarta-xmlbind-annotations-*", x);
+
+				break;
+			}
+		}
 
 		return content;
 	}
