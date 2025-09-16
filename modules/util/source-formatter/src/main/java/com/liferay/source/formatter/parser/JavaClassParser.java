aeddaeb4e9f55b3f17ddad3db5db76ca713e029a
Alan Huang <alan.huang@liferay.com>
2025-09-16 12:05:13 +0800

    Revert "LPD-65248 SF"
    This reverts commit 94456a304fda98678730e171c1a038c9f6b5d3af.

--- modules/util/source-formatter/src/main/java/com/liferay/source/formatter/parser/JavaClassParser.java
+++ modules/util/source-formatter/src/main/java/com/liferay/source/formatter/parser/JavaClassParser.java
@@ -416,6 +416,10 @@ public class JavaClassParser {
 			return javaClass;
 		}
 
+		if (detailAST.getType() == TokenTypes.STATIC_INIT) {
+			return new JavaStaticBlock(javaTermContent, detailAST.getLineNo());
+		}
+
 		String name = _getName(detailAST.findFirstToken(TokenTypes.IDENT));
 
 		if (detailAST.getType() == TokenTypes.CTOR_DEF) {
@@ -428,10 +432,6 @@ public class JavaClassParser {
 			return new JavaMethod(
 				accessModifier, javaTermContent, isAbstract, isFinal, isStatic,
 				detailAST.getLineNo(), name);
-		}
-
-		if (detailAST.getType() == TokenTypes.STATIC_INIT) {
-			return new JavaStaticBlock(javaTermContent, detailAST.getLineNo());
 		}
 
 		if (detailAST.getType() == TokenTypes.VARIABLE_DEF) {
