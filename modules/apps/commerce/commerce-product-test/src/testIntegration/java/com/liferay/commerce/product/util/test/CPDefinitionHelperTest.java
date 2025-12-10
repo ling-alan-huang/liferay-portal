e317bbbdbef4615ec28f711d4e69669244f819fe
Alan Huang <alan.huang@liferay.com>
2025-12-11 00:25:54 +0800

    LPD-43765 SF

--- modules/apps/commerce/commerce-product-test/src/testIntegration/java/com/liferay/commerce/product/util/test/CPDefinitionHelperTest.java
+++ modules/apps/commerce/commerce-product-test/src/testIntegration/java/com/liferay/commerce/product/util/test/CPDefinitionHelperTest.java
@@ -27,8 +27,6 @@ import com.liferay.portal.kernel.test.rule.AggregateTestRule;
 import com.liferay.portal.kernel.test.util.CompanyTestUtil;
 import com.liferay.portal.kernel.test.util.RandomTestUtil;
 import com.liferay.portal.kernel.test.util.ServiceContextTestUtil;
-import com.liferay.portal.kernel.util.ArrayUtil;
-import com.liferay.portal.kernel.util.ListUtil;
 import com.liferay.portal.kernel.util.LocaleUtil;
 import com.liferay.portal.kernel.workflow.WorkflowConstants;
 import com.liferay.portal.test.rule.Inject;
@@ -188,10 +186,8 @@ public class CPDefinitionHelperTest {
 			cpCatalogEntries,
 			cpCatalogEntry -> cpCatalogEntry.getCPDefinitionId());
 
-		Long[] cpDefinitionIdsLong = ArrayUtil.toArray(cpDefinitionIds);
-
-		List<Long> cpDefinitionIdsList = ListUtil.fromArray(
-			cpDefinitionIdsLong);
+		List<Long> cpDefinitionIdsList = TransformUtil.transformToList(
+			cpDefinitionIds, cpDefinitionId -> cpDefinitionId);
 
 		Assert.assertTrue(
 			actualCPDefinitionIds.containsAll(cpDefinitionIdsList));
