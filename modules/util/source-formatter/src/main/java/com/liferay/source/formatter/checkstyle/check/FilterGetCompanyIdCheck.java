18ef6a40968c7e641fd7c5fa5c8ff43f5f0682ea
Alan Huang <alan.huang@liferay.com>
2025-12-13 20:47:13 +0800

    s

--- modules/util/source-formatter/src/main/java/com/liferay/source/formatter/checkstyle/check/FilterGetCompanyIdCheck.java
+++ modules/util/source-formatter/src/main/java/com/liferay/source/formatter/checkstyle/check/FilterGetCompanyIdCheck.java
@@ -7,16 +7,21 @@ package com.liferay.source.formatter.checkstyle.check;
 
 import com.liferay.petra.string.StringPool;
 import com.liferay.portal.kernel.util.ArrayUtil;
+import com.liferay.source.formatter.check.util.BNDSourceUtil;
 import com.liferay.source.formatter.check.util.JavaSourceUtil;
 import com.liferay.source.formatter.check.util.SourceUtil;
 import com.liferay.source.formatter.util.FileUtil;
 import com.puppycrawl.tools.checkstyle.api.DetailAST;
+import com.puppycrawl.tools.checkstyle.api.FileContents;
+import com.puppycrawl.tools.checkstyle.api.FileText;
 import com.puppycrawl.tools.checkstyle.api.TokenTypes;
 
 import java.io.File;
 import java.util.List;
+import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+
 
 /**
  * @author Alan Huang
@@ -28,7 +33,7 @@ public class FilterGetCompanyIdCheck extends BaseCheck {
 		return new int[] {TokenTypes.CLASS_DEF};
 	}
 
-	protected boolean isLiferayFilter(String absolutePath, String content) {
+	protected boolean isBaseFilter(String absolutePath, String content) {
 		Pattern pattern = Pattern.compile(
 				" class " + JavaSourceUtil.getClassName(absolutePath) +
 						"\\s+extends\\s+([\\w.]+) ");
@@ -41,11 +46,7 @@ public class FilterGetCompanyIdCheck extends BaseCheck {
 
 		String extendedClassName = matcher.group(1);
 
-		if (extendedClassName.equals("UpgradeProcess")) {
-			List<String> importNames = getImportNames(detailAST);
-			if (importNames.contains()) {
-				
-			}
+		if (extendedClassName.equals("BaseFilter")) {
 			return true;
 		}
 
@@ -69,16 +70,47 @@ public class FilterGetCompanyIdCheck extends BaseCheck {
 
 		File file = JavaSourceUtil.getJavaFile(
 				extendedClassName, SourceUtil.getRootDirName(absolutePath),
-				getBundleSymbolicNamesMap(absolutePath));
+				_getBundleSymbolicNamesMap(absolutePath));
 
 		if (file == null) {
 			return false;
 		}
 
-		return isUpgradeProcess(file.getAbsolutePath(), FileUtil.read(file));
+		return isBaseFilter(file.getAbsolutePath(), FileUtil.read(file));
 	}
+	private synchronized Map<String, String> _getBundleSymbolicNamesMap(
+			String absolutePath) {
+
+		if (_bundleSymbolicNamesMap == null) {
+			_bundleSymbolicNamesMap = BNDSourceUtil.getBundleSymbolicNamesMap(
+					_getRootDirName(absolutePath));
+		}
+
+		return _bundleSymbolicNamesMap;
+	}
+	private volatile String _rootDirName;
+	private synchronized String _getRootDirName(String absolutePath) {
+		if (_rootDirName != null) {
+			return _rootDirName;
+		}
+
+		_rootDirName = SourceUtil.getRootDirName(absolutePath);
+
+		return _rootDirName;
+	}
+
+	private volatile Map<String, String> _bundleSymbolicNamesMap;
 	@Override
 	protected void doVisitToken(DetailAST detailAST) {
+
+		FileContents fileContents = getFileContents();
+
+		FileText fileText = fileContents.getText();
+
+		if (!isBaseFilter(getAbsolutePath(), (String)fileText.getFullText())) {
+			return;
+		}
+		
 		List<DetailAST> methodCallDetailASTs = getAllChildTokens(
 				detailAST, true, TokenTypes.METHOD_CALL);
 
@@ -96,17 +128,27 @@ public class FilterGetCompanyIdCheck extends BaseCheck {
 				continue;
 			}
 
-			String methodCallClassName = names.get(0);
 			String methodCallMethodName = names.get(1);
-
-			if (methodCallMethodName.equals("getCompanyId")) {
+			if (!methodCallMethodName.equals("getCompany") &&
+					!methodCallMethodName.equals("getCompanyId")) {
 				continue;
 			}
+
+			String methodCallClassName = names.get(0);
 			
-			if (!methodCallClassName.equals("PortalImpl") &&
-					!methodCallClassName.equals("PortalInstances") &&
-					!methodCallClassName.equals("PortalUtil")) {
+			if (methodCallClassName.equals("PortalInstances") ||
+					methodCallClassName.equals("PortalUtil")) {
+				log(
+						methodCallDetailAST, _MSG_AVOID_GET_COMPANY_ID_CALL,
+						methodCallClassName);
 				
+				continue;
+
+			}
+			
+			String typeName = getVariableTypeName(methodCallDetailAST, methodCallClassName, true);
+
+			if ((typeName == null) || !typeName.equals("PortalImpl")) {
 				continue;
 			}
 
@@ -114,11 +156,14 @@ public class FilterGetCompanyIdCheck extends BaseCheck {
 					methodCallDetailAST, _MSG_AVOID_GET_COMPANY_ID_CALL,
 					methodCallClassName);
 
+
 		}
 
 	}
 
 
+	private static final String _MSG_AVOID_GET_COMPANY_CALL =
+		"get.company.call.avoid";
 	private static final String _MSG_AVOID_GET_COMPANY_ID_CALL =
 		"get.company.id.call.avoid";
 
