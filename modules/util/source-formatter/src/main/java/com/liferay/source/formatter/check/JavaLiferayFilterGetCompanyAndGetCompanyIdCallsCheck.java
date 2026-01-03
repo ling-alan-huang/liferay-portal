d4dcbb1a2005c5bcea05b34b29832ad16838a9b2
Alan Huang <alan.huang@liferay.com>
2026-01-04 00:12:13 +0800

    s

--- modules/util/source-formatter/src/main/java/com/liferay/source/formatter/check/JavaLiferayFilterGetCompanyAndGetCompanyIdCallsCheck.java
+++ modules/util/source-formatter/src/main/java/com/liferay/source/formatter/check/JavaLiferayFilterGetCompanyAndGetCompanyIdCallsCheck.java
@@ -1,34 +1,25 @@
 /**
- * SPDX-FileCopyrightText: (c) 2000 Liferay, Inc. https://liferay.com
+ * SPDX-FileCopyrightText: (c) 2025 Liferay, Inc. https://liferay.com
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
 package com.liferay.source.formatter.check;
 
 import com.liferay.petra.string.StringBundler;
-import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portal.tools.ToolsUtil;
-import com.liferay.source.formatter.check.util.JavaSourceUtil;
-import com.liferay.source.formatter.parser.*;
-
-import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import com.liferay.source.formatter.parser.JavaClass;
+import com.liferay.source.formatter.parser.JavaTerm;
+import com.liferay.source.formatter.parser.JavaVariable;
 
 /**
  * @author Alan Huang
  */
-public class JavaLiferayFilterGetCompanyAndGetCompanyIdCallsCheck extends BaseJavaTermCheck {
+public class JavaLiferayFilterGetCompanyAndGetCompanyIdCallsCheck
+	extends BaseJavaTermCheck {
 
 	@Override
 	public boolean isLiferaySourceCheck() {
 		return true;
-	}
-
-	protected boolean isLiferayFilter(String absolutePath, String content) {
-		return _isDerivedFrom(
-				absolutePath, content,
-				"com.liferay.portal.kernel.servlet.BaseFilter");
 	}
 
 	@Override
@@ -37,14 +28,14 @@ public class JavaLiferayFilterGetCompanyAndGetCompanyIdCallsCheck extends BaseJa
 		String fileContent) {
 
 		if (!fileName.endsWith("Filter.java") ||
-				!isLiferayFilter(absolutePath, fileContent)) {
+			!isLiferayFilter(absolutePath, fileContent)) {
+
 			return javaTerm.getContent();
 		}
 
-		_checkGetCompanyCall(fileName, fileContent, "PortalUtil.getCompany");
-
-		_checkGetCompanyIdCall(fileName, fileContent, "PortalInstances.getCompanyId");
-		_checkGetCompanyIdCall(fileName, fileContent, "PortalUtil.getCompanyId");
+		_checkMethodCall(fileName, fileContent, "PortalInstances.getCompanyId");
+		_checkMethodCall(fileName, fileContent, "PortalUtil.getCompany");
+		_checkMethodCall(fileName, fileContent, "PortalUtil.getCompanyId");
 
 		JavaClass javaClass = (JavaClass)javaTerm;
 
@@ -58,16 +49,17 @@ public class JavaLiferayFilterGetCompanyAndGetCompanyIdCallsCheck extends BaseJa
 			String variableName = javaVariable.getName();
 
 			String variableTypeName = getVariableTypeName(
-					javaVariable.getContent(), childJavaTerm, fileContent, fileName,
-					variableName, true, false);
+				javaVariable.getContent(), childJavaTerm, fileContent, fileName,
+				variableName, true, false);
 
 			if (!variableTypeName.equals("Portal")) {
 				continue;
 			}
 
-			_checkGetCompanyCall(fileName, fileContent, variableName + ".getCompany");
-			_checkGetCompanyIdCall(fileName, fileContent, variableName + ".getCompanyId");
-
+			_checkMethodCall(
+				fileName, fileContent, variableName + ".getCompany");
+			_checkMethodCall(
+				fileName, fileContent, variableName + ".getCompanyId");
 		}
 
 		return javaTerm.getContent();
@@ -78,7 +70,27 @@ public class JavaLiferayFilterGetCompanyAndGetCompanyIdCallsCheck extends BaseJa
 		return new String[] {JAVA_CLASS};
 	}
 
-	private void _checkGetCompanyCall(String fileName, String content, String methodCall) {
+	protected boolean isLiferayFilter(String absolutePath, String content) {
+		return _isDerivedFrom(
+			absolutePath, content,
+			"com.liferay.portal.kernel.servlet.BaseFilter");
+	}
+
+	private void _checkMethodCall(
+		String fileName, String content, String methodCall) {
+
+		StringBundler sb = new StringBundler(5);
+
+		sb.append("Use \"PortalInstances.getCompanyId\" ");
+
+		if (methodCall.endsWith("getCompany")) {
+			sb.append("and \"CompanyLocalServiceUtil.fetchCompanyById\" ");
+		}
+
+		sb.append("instead of \"");
+		sb.append(methodCall);
+		sb.append("\"");
+
 		int x = -1;
 
 		while (true) {
@@ -92,148 +104,7 @@ public class JavaLiferayFilterGetCompanyAndGetCompanyIdCallsCheck extends BaseJa
 				continue;
 			}
 
-			addMessage(
-					fileName,
-					"Use \"PortalInstances.getCompanyId\" and \"CompanyLocalServiceUtil.fetchCompanyById\" instead of \"" + methodCall + "\"",
-					getLineNumber(content, x));
-		}
-	}
-	private void _checkGetCompanyIdCall(String fileName, String content, String methodCall) {
-
-		int x = -1;
-
-		while (true) {
-			x = content.indexOf(methodCall + "(", x + 1);
-
-			if (x == -1) {
-				return;
-			}
-
-			if (ToolsUtil.isInsideQuotes(content, x)) {
-				continue;
-			}
-
-			addMessage(
-					fileName,
-					"Use \"PortalInstances.getCompanyId\" instead of \"" + methodCall + "\"",
-					getLineNumber(content, x));
-		}
-
-	}
-	private void _checkAnnotationForMethod(
-		String fileName, JavaTerm javaTerm, String requiredMethodNameRegex,
-		boolean staticRequired, String... annotations) {
-
-		String methodName = javaTerm.getName();
-
-		Pattern pattern = Pattern.compile(requiredMethodNameRegex);
-
-		Matcher matcher = pattern.matcher(methodName);
-
-		boolean hasAnnotation = false;
-
-		for (String annotation : annotations) {
-			if (javaTerm.hasAnnotation(annotation)) {
-				hasAnnotation = true;
-
-				break;
-			}
-		}
-
-		if (hasAnnotation) {
-			if (!matcher.find()) {
-				addMessage(
-					fileName, "Incorrect method name \"" + methodName + "\"",
-					javaTerm.getLineNumber());
-			}
-			else if (javaTerm.isStatic() != staticRequired) {
-				addMessage(
-					fileName,
-					"Incorrect method type for \"" + methodName + "\"",
-					javaTerm.getLineNumber());
-			}
-
-			return;
-		}
-
-		if (!matcher.find()) {
-			return;
-		}
-
-		JavaSignature signature = javaTerm.getSignature();
-
-		List<JavaParameter> parameters = signature.getParameters();
-
-		if (!parameters.isEmpty()) {
-			return;
-		}
-
-		JavaClass javaClass = javaTerm.getParentJavaClass();
-
-		if (javaClass.isAnonymous()) {
-			return;
-		}
-
-		JavaClass parentJavaClass = javaClass.getParentJavaClass();
-
-		if (parentJavaClass == null) {
-			StringBundler sb = new StringBundler();
-
-			for (String annotation : annotations) {
-				sb.append("@");
-				sb.append(annotation);
-				sb.append(" or ");
-			}
-
-			sb.setIndex(sb.index() - 1);
-
-			addMessage(
-				fileName,
-				StringBundler.concat(
-					"Annotation ", sb, " required for \"", methodName, "\""),
-				javaTerm.getLineNumber());
-		}
-	}
-
-	private void _checkFeatureFlagsAnnotation(
-		String fileName, JavaTerm javaTerm) {
-
-		String javaTermContent = javaTerm.getContent();
-
-		int x = -1;
-
-		while (true) {
-			x = javaTermContent.indexOf("PropsUtil.addProperties(", x + 1);
-
-			if (x == -1) {
-				break;
-			}
-
-			List<String> parameterList = JavaSourceUtil.getParameterList(
-				JavaSourceUtil.getMethodCall(javaTerm.getContent(), x));
-
-			if ((parameterList.size() != 1) ||
-				!StringUtil.startsWith(
-					parameterList.get(0),
-					"UnicodePropertiesBuilder.setProperty(")) {
-
-				continue;
-			}
-
-			parameterList = JavaSourceUtil.getParameterList(
-				JavaSourceUtil.getMethodCall(parameterList.get(0), 0));
-
-			if ((parameterList.size() == 2) &&
-				StringUtil.startsWith(
-					parameterList.get(0), "\"feature.flag.")) {
-
-				addMessage(
-					fileName,
-					"Use annotation \"@FeatureFlags\" instead of \"PropsUtil." +
-						"addProperties\" for feature flag",
-					javaTerm.getLineNumber() +
-						getLineNumber(javaTermContent, x) - 1);
-			}
+			addMessage(fileName, sb.toString(), getLineNumber(content, x));
 		}
 	}
 
