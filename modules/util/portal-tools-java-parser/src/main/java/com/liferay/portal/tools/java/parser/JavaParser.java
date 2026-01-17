d800dd1abe94d7992c8cf4a56b3f7158142bfef2
Alan Huang <alan.huang@liferay.com>
2026-01-17 23:57:43 +0800

    Not needed

--- modules/util/portal-tools-java-parser/src/main/java/com/liferay/portal/tools/java/parser/JavaParser.java
+++ modules/util/portal-tools-java-parser/src/main/java/com/liferay/portal/tools/java/parser/JavaParser.java
@@ -355,11 +355,7 @@ public class JavaParser {
 			fileContents, startLineNumber, endLineNumber);
 
 		if (!actualJavaTermContent.startsWith(expectedJavaTermContent) &&
-			!_isExcludedJavaTerm(parsedJavaTerm) &&
-			(!actualJavaTermContent.contains("\n\n") ||
-			 !Objects.equals(
-				 parsedJavaTerm.getClassName(),
-				 JavaTryStatement.class.getName()))) {
+			!_isExcludedJavaTerm(parsedJavaTerm)) {
 
 			contentModifications.addReplaceContent(
 				parsedJavaTerm.getContent(), startPosition.getLineNumber(),
