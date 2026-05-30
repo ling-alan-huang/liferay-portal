c31fe5c95432e35af449fb796f149001c7218d56
Alan Huang <alan.huang@liferay.com>
2026-05-30 23:24:05 +0800

    LPD-91944 Move logic

--- modules/util/source-formatter/src/main/java/com/liferay/source/formatter/check/YMLStylingCheck.java
+++ modules/util/source-formatter/src/main/java/com/liferay/source/formatter/check/YMLStylingCheck.java
@@ -8,7 +8,10 @@ package com.liferay.source.formatter.check;
 import com.liferay.petra.io.unsync.UnsyncBufferedReader;
 import com.liferay.petra.io.unsync.UnsyncStringReader;
 import com.liferay.petra.string.CharPool;
+import com.liferay.petra.string.StringBundler;
 import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portal.kernel.util.Validator;
 import com.liferay.source.formatter.check.util.SourceUtil;
@@ -37,11 +40,9 @@ public class YMLStylingCheck extends BaseFileCheck {
 		}
 
 		content = content.replaceAll(
-			"(\\A|\n)( *)(description:) (?!\\|-)(.+)(\\Z|\n)",
-			"$1$2$3\n    $2$4$5");
-		content = content.replaceAll("(\\A|\n) *description:\n +\"\"", "");
-		content = content.replaceAll(
 			"(\\A|\n)( *#)@? ?(review)(\\Z|\n)", "$1$2 @$3$4");
+
+		content = _formatDescription(fileName, absolutePath, content);
 
 		return _formatQuotes(content);
 	}
@@ -122,6 +123,108 @@ public class YMLStylingCheck extends BaseFileCheck {
 		}
 
 		return s;
+	}
+
+	private String _formatDescription(
+		String fileName, String absolutePath, String content) {
+
+		content = content.replaceAll(
+			"(\\A|\n)( *)(description:) (?!\\|-)(.+)(\\Z|\n)",
+			"$1$2$3\n    $2$4$5");
+		content = content.replaceAll("(\\A|\n) *description:\n +\"\"", "");
+
+		int maxLineLength = 0;
+
+		try {
+			maxLineLength = Integer.parseInt(
+				getAttributeValue(_MAX_LINE_LENGTH, absolutePath));
+		}
+		catch (NumberFormatException numberFormatException) {
+			if (_log.isDebugEnabled()) {
+				_log.debug(numberFormatException);
+			}
+
+			return content;
+		}
+
+		StringBundler sb = new StringBundler();
+
+		String description = null;
+		String descriptionLeadingSpaces = StringPool.BLANK;
+		String leadingSpaces = StringPool.BLANK;
+
+		for (String line : content.split("\n")) {
+			if (description == null) {
+				String trimmedLine = line.trim();
+
+				if (!trimmedLine.startsWith("description:")) {
+					sb.append(line);
+					sb.append(StringPool.NEW_LINE);
+
+					continue;
+				}
+
+				descriptionLeadingSpaces = SourceUtil.getLeadingSpaces(line);
+
+				sb.append(descriptionLeadingSpaces);
+
+				sb.append("description:");
+				sb.append(StringPool.NEW_LINE);
+
+				description = StringPool.BLANK;
+
+				continue;
+			}
+
+			leadingSpaces = SourceUtil.getLeadingSpaces(line);
+
+			if (leadingSpaces.length() > descriptionLeadingSpaces.length()) {
+				description = description + StringPool.SPACE + line.trim();
+
+				continue;
+			}
+
+			if (!description.isEmpty()) {
+				String indent =
+					descriptionLeadingSpaces + StringPool.FOUR_SPACES;
+
+				description = indent + description.trim();
+
+				if (!fileName.endsWith("/rest-openapi.yaml")) {
+					description = _splitDescription(
+						description, indent, maxLineLength);
+				}
+
+				sb.append(description);
+				sb.append(StringPool.NEW_LINE);
+			}
+
+			sb.append(line);
+			sb.append(StringPool.NEW_LINE);
+
+			description = null;
+			descriptionLeadingSpaces = StringPool.BLANK;
+		}
+
+		if (!Validator.isBlank(description)) {
+			String indent = descriptionLeadingSpaces + StringPool.FOUR_SPACES;
+
+			description = indent + description.trim();
+
+			if (!fileName.endsWith("/rest-openapi.yaml")) {
+				description = _splitDescription(
+					description, indent, maxLineLength);
+			}
+
+			sb.append(description);
+			sb.append(StringPool.NEW_LINE);
+		}
+
+		if (sb.index() > 0) {
+			sb.setIndex(sb.index() - 1);
+		}
+
+		return sb.toString();
 	}
 
 	private String _formatQuotes(String content) throws IOException {
@@ -231,5 +334,40 @@ public class YMLStylingCheck extends BaseFileCheck {
 
 		return false;
 	}
+
+	private String _splitDescription(
+		String description, String indent, int maxLineLength) {
+
+		if (description.length() <= maxLineLength) {
+			return description;
+		}
+
+		int pos = description.indexOf(CharPool.SPACE, indent.length());
+
+		if (pos == -1) {
+			return description;
+		}
+
+		if (pos > maxLineLength) {
+			return StringBundler.concat(
+				description.substring(0, pos), StringPool.NEW_LINE,
+				_splitDescription(
+					indent + description.substring(pos + 1), indent,
+					maxLineLength));
+		}
+
+		pos = description.lastIndexOf(CharPool.SPACE, maxLineLength);
+
+		return StringBundler.concat(
+			description.substring(0, pos), StringPool.NEW_LINE,
+			_splitDescription(
+				indent + description.substring(pos + 1), indent,
+				maxLineLength));
+	}
+
+	private static final String _MAX_LINE_LENGTH = "maxLineLength";
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		YMLStylingCheck.class);
 
 }
