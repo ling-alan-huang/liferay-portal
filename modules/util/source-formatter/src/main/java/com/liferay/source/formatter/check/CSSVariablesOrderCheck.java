33fdbda7d8b559b2d2892002df55b772edd44c5b
Alan Huang <alan.huang@liferay.com>
2026-07-06 01:03:31 +0800

    LPD-80363 Not needed

--- modules/util/source-formatter/src/main/java/com/liferay/source/formatter/check/CSSVariablesOrderCheck.java
+++ modules/util/source-formatter/src/main/java/com/liferay/source/formatter/check/CSSVariablesOrderCheck.java
@@ -12,7 +12,6 @@ import com.liferay.portal.kernel.util.NaturalOrderStringComparator;
 import com.liferay.portal.kernel.util.Tuple;
 import com.liferay.portal.kernel.util.Validator;
 
-import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -42,8 +41,7 @@ public class CSSVariablesOrderCheck extends BaseFileCheck {
 			}
 
 			_checkPropertiesOrder(
-				fileName, absolutePath, variableName,
-				(String)variableDeclarationTuple.getObject(0),
+				fileName, (String)variableDeclarationTuple.getObject(0),
 				getLineNumber(content, matcher1.start()));
 
 			int endIndex = (int)variableDeclarationTuple.getObject(1);
@@ -110,17 +108,7 @@ public class CSSVariablesOrderCheck extends BaseFileCheck {
 	}
 
 	private void _checkPropertiesOrder(
-		String fileName, String absolutePath, String variableName, String s,
-		int lineNumber) {
-
-		List<String> skipSortVariableNames = getAttributeValues(
-			_SKIP_SORT_VARIABLE_NAMES_KEY, absolutePath);
-
-		for (String skipSortVariableName : skipSortVariableNames) {
-			if (variableName.contains(skipSortVariableName)) {
-				return;
-			}
-		}
+		String fileName, String s, int lineNumber) {
 
 		Matcher matcher1 = _propertyKeyPattern.matcher(s);
 
@@ -237,9 +225,6 @@ public class CSSVariablesOrderCheck extends BaseFileCheck {
 
 		return false;
 	}
-
-	private static final String _SKIP_SORT_VARIABLE_NAMES_KEY =
-		"skipSortVariableNames";
 
 	private static final NaturalOrderStringComparator _comparator =
 		new NaturalOrderStringComparator();
