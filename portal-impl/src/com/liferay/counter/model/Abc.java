ebd16a4704cc8c24c9989955e9a955169c98fa2e
Alan Huang <alan.huang@liferay.com>
2026-08-22 00:28:47 +0800

    test

---
+++ portal-impl/src/com/liferay/counter/model/Abc.java
@@ -0,0 +1,33 @@
+package com.liferay.counter.model;
+
+import java.util.List;
+
+public class Abc {
+
+	int inta;
+	List<String> list;
+	Object o;
+	String[] s;
+	A.B.C.String[] s1;
+	double[] abc;
+	double[][] xyz;
+	double[] aaa = {3, 4};
+	String[] s2 = {"a1", "a2", "a3", "a4"};
+
+	private com.liferay.portal.search.sort.Sort[] _a() {
+		Mockito.any(Sort[].class);
+		Mockito.any(BlogsEntry[]::new);
+		Mockito.any(ArrayList<?>[]::new);
+
+		return new com.liferay.portal.search.sort.Sort[] {a, b};
+	}
+
+	private static final String[][] _RESOURCE_NAMES = new String[30][20];
+
+	Long accountEntryId = (Long)values[0][1][2];
+
+	private final Map<Long, long[]> _accountGroupIdsMap = new HashMap<>();
+
+	BooleanClause<Query>[] booleanClauses = searchContext.getBooleanClauses();
+
+}
