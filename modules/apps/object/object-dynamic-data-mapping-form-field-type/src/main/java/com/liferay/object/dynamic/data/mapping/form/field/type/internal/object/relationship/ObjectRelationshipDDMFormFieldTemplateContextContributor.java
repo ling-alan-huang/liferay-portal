c1d8c373b4b0fb5c91c81a257a7ba12232a0fc63
Alan Huang <alan.huang@liferay.com>
2026-09-18 19:14:30 +0800

    LPD-105896 Auto SF, sort Java terms

--- modules/apps/object/object-dynamic-data-mapping-form-field-type/src/main/java/com/liferay/object/dynamic/data/mapping/form/field/type/internal/object/relationship/ObjectRelationshipDDMFormFieldTemplateContextContributor.java
+++ modules/apps/object/object-dynamic-data-mapping-form-field-type/src/main/java/com/liferay/object/dynamic/data/mapping/form/field/type/internal/object/relationship/ObjectRelationshipDDMFormFieldTemplateContextContributor.java
@@ -141,20 +141,6 @@ public class ObjectRelationshipDDMFormFieldTemplateContextContributor
 		return valueString;
 	}
 
-	private String _getAdditionalAPIURLParameters(
-		ObjectDefinition objectDefinition) {
-
-		SystemObjectDefinitionManager systemObjectDefinitionManager =
-			_systemObjectDefinitionManagerRegistry.
-				getSystemObjectDefinitionManager(objectDefinition.getName());
-
-		if (systemObjectDefinitionManager == null) {
-			return StringPool.BLANK;
-		}
-
-		return systemObjectDefinitionManager.getAdditionalAPIURLParameters();
-	}
-
 	private String _getAPIURL(
 		DDMFormField ddmFormField,
 		DDMFormFieldRenderingContext ddmFormFieldRenderingContext) {
@@ -192,6 +178,20 @@ public class ObjectRelationshipDDMFormFieldTemplateContextContributor
 		}
 
 		return apiURL;
+	}
+
+	private String _getAdditionalAPIURLParameters(
+		ObjectDefinition objectDefinition) {
+
+		SystemObjectDefinitionManager systemObjectDefinitionManager =
+			_systemObjectDefinitionManagerRegistry.
+				getSystemObjectDefinitionManager(objectDefinition.getName());
+
+		if (systemObjectDefinitionManager == null) {
+			return StringPool.BLANK;
+		}
+
+		return systemObjectDefinitionManager.getAdditionalAPIURLParameters();
 	}
 
 	private long _getGroupId(
